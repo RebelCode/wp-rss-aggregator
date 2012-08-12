45249d43b65dca2863fb24b3b9261b107ee29089
Jean Galea <jean@jpgalea.com>
2012-08-12 19:00:08 +0200

    finalised new free version, branching back

--- wp-rss-aggregator.php
+++ wp-rss-aggregator.php
@@ -25,283 +25,63 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
+    // Define constants for ease of updating/modification later on in the lifespan of the plugin
+    define( 'WPRSS_VERSION', '1.1', true );
+    define( 'WPRSS_JS',   plugins_url( 'js',  __FILE__ ) , true );
+    define( 'WPRSS_CSS',  plugins_url( 'css', __FILE__ ) , true );
+    define( 'WPRSS_IMG',  plugins_url( 'img', __FILE__ ) , true );
+    define( 'WPRSS_INC',  plugins_url( 'inc', __FILE__ ) , true );
+    define( 'WPRSS_PATH', plugin_dir_path( __FILE__ ), true );
+
 
     /**
-     * Plugin activation procedure
-     */      
-    
-    register_activation_hook( __FILE__, 'wprss_install' );    
-    
-    function wprss_install() {
-        // Activates the plugin and checks for compatible version of WordPress 
-        if ( version_compare( get_bloginfo( 'version' ), '2.9', '<' ) ) {
-            deactivate_plugins ( basename( __FILE__ ));     // Deactivate plugin
-            wp_die( "This plugin requires WordPress version 2.9 or higher." );
-        }
-          
-        if ( !wp_next_scheduled( 'wprss_generate_hook' ) ) {            
-            // Schedule to run hourly
-            wp_schedule_event( time(), 'hourly', 'wprss_generate_hook' );
-        }
-        
-        add_action( 'wprss_generate_hook', 'wp_rss_aggregator' );                
-    }
-    
-    
-    /**
-     * Plugin deactivation procedure
-     */    
-    
-    register_deactivation_hook( __FILE__, 'wprss_deactivate' );
-    
-    function wprss_deactivate() {
-        // on deactivation remove the cron job 
-        if ( wp_next_scheduled( 'wprss_generate_hook' ) ) 
-        wp_clear_scheduled_hook( 'wprss_generate_hook' );
-    }
-     
-    
-    /**
-     * Actions for plugin's options page
+     * Initialisation of the plugin
      */
-    
-    // Only load scripts if we are on this plugin's options page (admin)
-    if ( isset( $_GET['page'] ) && $_GET['page'] == 'wprss-aggregator'  ) {
-        add_action( 'admin_print_scripts', 'wprss_register_scripts' );
-    }
 
-    // Only load scripts if we are on this plugin's options or settings pages (admin)
-    if ( isset( $_GET['page'] ) && ( $_GET['page'] == 'wprss-aggregator' | $_GET['page'] == 'wprss-aggregator-settings' ) ) {        
-        add_action( 'admin_print_styles', 'wprss_header' );
-    }    
-  
+    add_action( 'init', 'wprss_init' );
 
-    /**
-     * Include scripts in plugin page header (admin)
-     */     
-    
-    function wprss_register_scripts() {
-         wp_enqueue_script( 'jquery' );
-         wp_enqueue_script( 'add-remove', plugins_url( 'js/add-remove.js', __FILE__) );
+    function wprss_init() {
+        
+        require ( WPRSS_PATH . 'inc/activation.php' );
+        require ( WPRSS_PATH . 'inc/deactivation.php' );
+        require ( WPRSS_PATH . 'inc/shortcodes.php' );
+        require ( WPRSS_PATH . 'inc/admin-options.php' );
+        
+        register_activation_hook( WPRSS_INC . '/activation.php', 'wprss_activate' );
+        register_deactivation_hook( WPRSS_INC . '/deactivation.php', 'wprss_deactivate' );
+        
+        add_action ( 'wp_head', 'wprss_head_output' );   
+
+        wp_enqueue_style( 'colorbox', WPRSS_CSS . '/colorbox.css' );
+        wp_enqueue_script( 'jquery.colorbox-min', WPRSS_JS .'/jquery.colorbox-min.js', array('jquery') );          
+             
+        // Only load scripts if we are on this plugin's options or settings pages (admin)
+        if ( isset( $_GET['page'] ) && ( $_GET['page'] == 'wprss-aggregator' | $_GET['page'] == 'wprss-aggregator-settings' ) ) {        
+            wp_enqueue_style( 'styles', WPRSS_CSS . '/styles.css' );
+        } 
+   
+        // Only load scripts if we are on this plugin's options page (admin)
+        if ( isset( $_GET['page'] ) && $_GET['page'] == 'wprss-aggregator' ) {
+           wp_enqueue_script( 'jquery' );
+            wp_enqueue_script( 'add-remove', WPRSS_JS . '/add-remove.js' );
+        }    
     }
-    
-    
+ 
+   
     /**
-     * Include Colorbox-related script and CSS in WordPress head on frontend
-     */      
-    
-    add_action( 'wp_enqueue_scripts', 'wprss_frontend_scripts' );
-    
-    function wprss_frontend_scripts() {
-         wp_enqueue_style( 'styles', plugins_url( 'includes/css/colorbox.css', __FILE__) );
-         wp_enqueue_script( 'jquery.colorbox-min', plugins_url( 'includes/scripts/jquery.colorbox-min.js', __FILE__) );         
-    }    
-    
-    
-    /**
-     * Output JQuery command to trigger Colorbox for links in the <head>
-     */  
-    
-    add_action ( 'wp_head', 'wprss_head_output' );
-    
+     * jQuery code to trigger Colorbox for links, goes in the <head> section on frontend
+     */
+
     function wprss_head_output() {
-        echo "<script type='text/javascript'>jQuery(document).ready(function(){ jQuery('a.colorbox').colorbox({iframe:true, width:'80%', height:'80%'});});</script>";
+        echo "
+        <script type='text/javascript'>
+            jQuery( document ).ready( function() { 
+                jQuery( 'a.colorbox' ).colorbox(
+                {iframe:true, width:'80%', height:'80%'}
+                );
+            });
+        </script>";
     }
-
-
-    /**
-     * Include CSS in plugin page header
-     */ 
-    
-    function wprss_header() {        
-        wp_enqueue_style( 'styles', plugins_url( 'includes/css/styles.css', __FILE__) );
-    }  
-    
-    
-    /**
-     * Plugin administration page
-     * 
-     * Note: Wording of options and settings is confusing, due to the plugin originally only having 
-     * an 'options' page to enter feed sources, and now needing two screens, one for feed sources and one for 
-     * general settings. Might implement something cleaner in the future.
-     */ 
-    
-    // Add the admin options and settings pages
-
-    add_action( 'admin_menu', 'wprss_add_page' );
-    
-    function wprss_add_page() {        
-        add_menu_page( 'RSS Aggregator', 'RSS Aggregator', 'manage_options', 'wprss-aggregator', 
-                       'wprss_options_page', plugins_url( '/includes/images/icon-adminmenu16-sprite.png', __FILE__ ) );
-
-        add_submenu_page( 'wprss-aggregator', 'WP RSS Aggregator Settings', 'Settings', 'manage_options', 
-                          'wprss-aggregator-settings', 'wprss_settings_page' );        
-    }    
-
-
-    /**
-     * Register and define options and settings
-     */ 
-    
-    add_action( 'admin_init', 'wprss_admin_init' );
-    
-    function wprss_admin_init() {
-        register_setting( 'wprss_options', 'wprss_options' );    
-        add_settings_section( 'wprss_main', '', 'wprss_section_text', 'wprss' );       
-
-        register_setting( 'wprss_settings', 'wprss_settings' );
-        
-
-        add_settings_section( 'wprss-settings-main', '', 'wprss_settings_section_text', 'wprss-aggregator-settings' );   
-        
-        add_settings_field( 'wprss-settings-open-dd', 'Open links behaviour', 
-                            'wprss_setting_open_dd', 'wprss-aggregator-settings', 'wprss-settings-main');
-
-        add_settings_field( 'wprss-settings-follow-dd', 'Set links as', 
-                            'wprss_setting_follow_dd', 'wprss-aggregator-settings', 'wprss-settings-main');        
-    }  
-
-    
-    /**
-     * Draw options page, used for adding feeds 
-     */ 
-    
-    function wprss_options_page() {
-        ?>
-        <div class="wrap">
-            <?php screen_icon( 'wprss-aggregator' ); ?>
-        
-         
-            <h2>WP RSS Aggregator Feed Sources</h2>
-            <div id="options">
-                <form action="options.php" method="post">            
-                    <?php
-                    
-                    settings_fields( 'wprss_options' );
-                    do_settings_sections( 'wprss' );
-                    $options = get_option( 'wprss_options' );        
-                    if ( !empty($options) ) {
-                        $size = count($options);
-                        for ( $i = 1; $i <= $size; $i++ ) {            
-                            if( $i % 2 == 0 ) continue;
-                            echo "<div class='wprss-input'>";
-                            
-                            $key = key( $options );
-                            
-                            echo "<p><label class='textinput' for='$key'>" . wprss_convert_key( $key ) . "</label>
-                            <input id='$key' class='wprss-input' size='100' name='wprss_options[$key]' type='text' value='$options[$key]' /></p>";
-                            
-                            next( $options );
-                            
-                            $key = key( $options );
-                            
-                            echo "<p><label class='textinput' for='$key'>" . wprss_convert_key( $key ) . "</label>
-                            <input id='$key' class='wprss-input' size='100' name='wprss_options[$key]' type='text' value='$options[$key]' /></p>";
-                            
-                            next( $options );
-                            echo "</div>"; 
-                            
-                        }
-                    }
-                    
-                    $images_url = plugins_url( 'includes/images', __FILE__); 
-                    ?>
-                    <div id="buttons"><a href="#" id="add"><img src="<?php echo $images_url; ?>/add.png"></a>  
-                    <a href="#" id="remove"><img src="<?php echo $images_url; ?>/remove.png"></a></div>  
-                    <p class="submit"><input type="submit" value="Save Settings" name="submit" class="button-primary"></p>
-                
-                </form>
-            </div> <!-- end options -->
-        </div> <!-- end wrap -->
-        <?php 
-    }
-    
-    
-  
-
-
-    /**
-     * Explain the options section
-     */ 
-    
-    function wprss_section_text() {
-        echo '<p>Enter a name and URL for each of your feeds. The name is used just for your reference.</p>';
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /**
-     * Build the plugin settings page, used to save general settings like whether a link should be follow or no follow
-     */ 
-
-    function wprss_settings_page() {
-        ?>
-        <div class="wrap">
-            <?php screen_icon( 'wprss-aggregator' ); ?>
-        
-            <h2>WP RSS Aggregator Settings</h2>
-            
-            <form action="options.php" method="post">            
-                <?php settings_fields( 'wprss_settings' ) ?>
-                <?php do_settings_sections( 'wprss-aggregator-settings' ); ?>
-                <p class="submit"><input type="submit" value="Save Settings" name="submit" class="button-primary"></p>
-            </form>
-        </div>
-        <?php
-    }
-
-
-    // Draw the section header
-    function wprss_settings_section_text() {
-   //     echo '<p>Enter your settings here.</p>';
-    }
-
-    // Follow or No Follow Dropdown
-    function wprss_setting_follow_dd() {
-        $options = get_option( 'wprss_settings' );
-        $items = array( "No follow", "Follow" );
-        echo "<select id='follow-dd' name='wprss_settings[follow_dd]'>";
-        foreach( $items as $item ) {
-            $selected = ( $options['follow_dd'] == $item) ? 'selected="selected"' : '';
-            echo "<option value='$item' $selected>$item</option>";
-        }
-        echo "</select>";
-    }
-
-    // Link open setting Dropdown
-    function wprss_setting_open_dd() {
-        $options = get_option( 'wprss_settings' );
-        $items = array( "Lightbox", "New window", "None" );
-        echo "<select id='open-dd' name='wprss_settings[open_dd]'>";
-        foreach( $items as $item ) {
-            $selected = ( $options['open_dd'] == $item) ? 'selected="selected"' : '';
-            echo "<option value='$item' $selected>$item</option>";
-        }
-        echo "</select>";
-    }
-
-    
-    
-
-
-
-
-
-
-
-
-
 
 
 
@@ -321,23 +101,6 @@
     }
     
     
-    /**
-     * Set up shortcodes and call the main function for output
-     */     
-    
-    // Register a new shortcode
-    add_shortcode( 'wp_rss_aggregator', 'wprss_shortcode');
-    
-    function wprss_shortcode( $atts ) {    
-        if ( !empty ($atts) ) {
-            foreach ( $atts as $key => &$val ) {
-                $val = html_entity_decode($val);
-            }
-        }
-        wp_rss_aggregator( $atts );       
-    }
-
-
     /**
      * Get feeds and output the aggregation
      */     
@@ -463,6 +226,7 @@
                 echo $link_after;
             }           
             echo $links_after;
+
         }
     }
     
