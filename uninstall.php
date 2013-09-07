a5c09b018e0a90a67d27e5c9ddcc4addd7ec44ce
Jean <jean@jpgalea.com>
2013-09-07 09:33:59 +0200

    minor changes to 3.3.2

--- uninstall.php
+++ uninstall.php
@@ -3,8 +3,11 @@
 if( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 exit ();
 
-// Delete option from options table
-wprss_remove_caps();
+// Remove capabilities
+if ( function_exists( wprss_remove_caps() )
+	wprss_remove_caps();
+
+// Delete option from options table	
 delete_option( 'wprss_options' );
 delete_option( 'wprss_settings' );
 delete_option( 'wprss_settings_general' );
