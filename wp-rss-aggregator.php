145d32f5dc90066d62fa79d0722385cb801e18e1
Jean <jean@jpgalea.com>
2013-09-13 17:38:13 +0200

    Preparing for 3.4 release, changing variables and read me etc.

--- wp-rss-aggregator.php
+++ wp-rss-aggregator.php
@@ -3,7 +3,7 @@
     Plugin Name: WP RSS Aggregator
     Plugin URI: http://www.wprssaggregator.com
     Description: Imports and aggregates multiple RSS Feeds using SimplePie
-    Version: 3.3.3
+    Version: 3.4
     Author: Jean Galea
     Author URI: http://www.wprssaggregator.com
     License: GPLv3
@@ -28,7 +28,7 @@
 
     /**
      * @package   WPRSSAggregator
-     * @version   3.3.3
+     * @version   3.4
      * @since     1.0
      * @author    Jean Galea <info@jeangalea.com>
      * @copyright Copyright (c) 2012-2013, Jean Galea
@@ -42,7 +42,7 @@
 
     // Set the version number of the plugin. 
     if( !defined( 'WPRSS_VERSION' ) )
-        define( 'WPRSS_VERSION', '3.3.3', true );
+        define( 'WPRSS_VERSION', '3.4', true );
 
     // Set the database version number of the plugin. 
     if( !defined( 'WPRSS_DB_VERSION' ) )
