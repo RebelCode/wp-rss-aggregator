d0029456ab48c6fde354bc6e799092ce7280fefc
Miguel Muscat <miguelmuscat93@gmail.com>
2013-12-01 13:27:15 +0100

    Shortcode disabled message

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -10,9 +10,10 @@
                 $val = html_entity_decode($val);
             }
         }      
-        ob_start(); //start an output buffer to output of the following function
+        ob_start(); // start an output buffer to output of the following function
         wprss_display_feed_items( $atts ); 
-        return ob_get_clean(); //return the current buffer and clear it
+        $feed_items = ob_get_clean(); // save the current buffer and clear it
+        return apply_filters( 'wprss_shortcode_output', $feed_items );
     }
     
     // Register shortcodes
