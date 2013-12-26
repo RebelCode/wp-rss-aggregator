df9a5f5196f788171e05df3a225342ac1639583f
Miguel Muscat <miguelmuscat93@gmail.com>
2013-12-26 18:38:22 +0100

    Fixed bug with feed item count column

--- includes/feed-processing.php
+++ includes/feed-processing.php
@@ -119,8 +119,13 @@
                 'cache_results' => false,   // Disable caching, used for one-off queries
                 'no_found_rows' => true,    // We don't need pagination, so disable it
                 'posts_per_page'=> -1,
-                'meta_key'      => 'wprss_feed_id',
-                'meta_value'    => $source_id,
+                'meta_query'    => array(
+                    array(
+                        'key'       => 'wprss_feed_id',
+                        'value'     => $source_id,
+                        'compare'   => 'LIKE',
+                    ),
+                )
             )
         );
         return new WP_Query( $args );
