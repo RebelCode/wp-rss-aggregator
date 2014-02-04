9ab830dc2881eff4b3f1a6c415c067c7cacf4fe9
Miguel Muscat <miguelmuscat93@gmail.com>
2014-02-04 14:16:51 +0100

    further divided feed-processing

--- includes/feed-importing.php
+++ includes/feed-importing.php
@@ -7,44 +7,69 @@
 
 
 
+	add_action( 'wprss_fetch_single_feed_hook', 'wprss_fetch_insert_single_feed_items' );
 	/**
-	 * A clone of the function 'fetch_feed' in wp-includes/feed.php [line #529]
+	 * The main feed fetching function.
+	 * Fetches the feed items from the source provided and inserts them into the DB.
 	 *
-	 * @since 3.5
+	 * Called on hook 'wprss_fetch_single_feed_hook'.
+	 *
+	 * @since 3.2
 	 */
-	function wprss_fetch_feed( $url ) {
-		require_once ( ABSPATH . WPINC . '/class-feed.php' );
-
-		$feed = new SimplePie();
-
-		// Commented out Sanitization, due to a conflict with google RSS image URLS.
-		// With sanitization on, the urls get truncated from the front.
-
-		// $feed->set_sanitize_class( 'WP_SimplePie_Sanitize_KSES' );
-		// We must manually overwrite $feed->sanitize because SimplePie's
-		// constructor sets it before we have a chance to set the sanitization class
-		// $feed->sanitize = new WP_SimplePie_Sanitize_KSES();
-
-		$feed->set_cache_class( 'WP_Feed_Cache' );
-		$feed->set_file_class( 'WP_SimplePie_File' );
-
-		$feed->set_feed_url( $url );
-
-		$feed->set_cache_duration( apply_filters( 'wp_feed_cache_transient_lifetime', 12 * HOUR_IN_SECONDS, $url ) );
-		do_action_ref_array( 'wp_feed_options', array( &$feed, $url ) );
-		$feed->init();
-		$feed->handle_content_type();
-
-		if ( $feed->error() ) {
-			return new WP_Error( 'simplepie-error', $feed->error() );
+	function wprss_fetch_insert_single_feed_items( $feed_ID ) {
+		// Check if the feed source is active.
+		if ( wprss_is_feed_source_active( $feed_ID ) === FALSE && wprss_feed_source_force_next_fetch( $feed_ID ) === FALSE ) {
+			// If it is not active ( paused ), return without fetching the feed items.
+			return;
+		}
+		// If the feed source is forced for next fetch, remove the force next fetch data
+		if ( wprss_feed_source_force_next_fetch( $feed_ID ) ) {
+			delete_post_meta( $feed_ID, 'wprss_force_next_fetch' );
 		}
 
-		return $feed;
+		// Get the feed source URL from post meta, and filter it
+		$feed_url = get_post_meta( $feed_ID, 'wprss_url', true );
+		$feed_url = apply_filters( 'wprss_feed_source_url', $feed_url, $feed_ID );
+
+		// Get the feed limit from post meta
+		$feed_limit = get_post_meta( $feed_ID, 'wprss_limit', true );
+		// Sanitize the limit. If smaller or equal to zero, or an empty string, set to NULL.
+		$feed_limit = ( $feed_limit <= 0 || empty( $feed_limit ) )? NULL : $feed_limit;
+
+		// Filter the URL for validaty
+		if ( filter_var( $feed_url, FILTER_VALIDATE_URL ) ) {
+			// Get the feed items from the source
+			$items = wprss_get_feed_items( $feed_url );
+			// If got NULL, convert to an empty array
+			if ( $items === NULL ) $items = array();
+
+			// If the feed has its own meta limit,
+			if ( $feed_limit !== NULL ) {
+				// slice the items array using the feed meta limit
+				// @todo - Check current number of feed items for source, and delete oldest to make room for new, to keep to the limit.
+				// 		   ( use wprss_get_feed_items_for_source )
+				$items_to_insert = array_slice( $items, 0, $feed_limit );
+			}
+			else { 
+				$items_to_insert = $items;
+			}
+			
+			// Insert the items into the db
+			if ( !empty( $items_to_insert ) ) {
+				wprss_items_insert_post( $items_to_insert, $feed_ID );
+			}
+		}
 	}
 
 
+
+
+
+
 	/**
-	 * Fetch the feeds from a feed item url
+	 * Fetches the feed items from a feed at the given URL.
+	 *
+	 * Called from 'wprss_fetch_insert_single_feed_items'
 	 *
 	 * @since 3.0
 	 */
@@ -89,8 +114,54 @@
 	}
 
 
+
+
+
+
+
 	/**
-	 * Insert a WPRSS feed item post
+	 * A clone of the function 'fetch_feed' in wp-includes/feed.php [line #529]
+	 *
+	 * Called from 'wprss_get_feed_items'
+	 *
+	 * @since 3.5
+	 */
+	function wprss_fetch_feed( $url ) {
+		require_once ( ABSPATH . WPINC . '/class-feed.php' );
+
+		$feed = new SimplePie();
+
+		// Commented out Sanitization, due to a conflict with google RSS image URLS.
+		// With sanitization on, the urls get truncated from the front.
+
+		// $feed->set_sanitize_class( 'WP_SimplePie_Sanitize_KSES' );
+		// We must manually overwrite $feed->sanitize because SimplePie's
+		// constructor sets it before we have a chance to set the sanitization class
+		// $feed->sanitize = new WP_SimplePie_Sanitize_KSES();
+
+		$feed->set_cache_class( 'WP_Feed_Cache' );
+		$feed->set_file_class( 'WP_SimplePie_File' );
+
+		$feed->set_feed_url( $url );
+
+		$feed->set_cache_duration( apply_filters( 'wp_feed_cache_transient_lifetime', 12 * HOUR_IN_SECONDS, $url ) );
+		do_action_ref_array( 'wp_feed_options', array( &$feed, $url ) );
+		$feed->init();
+		$feed->handle_content_type();
+
+		if ( $feed->error() ) {
+			return new WP_Error( 'simplepie-error', $feed->error() );
+		}
+
+		return $feed;
+	}
+
+
+
+
+
+	/**
+	 * Insert wprss_feed_item posts into the DB
 	 *
 	 * @since 3.0
 	 */
@@ -200,8 +271,13 @@
 	}
 
 
+
+
+
 	/**
-	 * Creates meta entries for feed items while they are being imported
+	 * Inserts the appropriate post meta for feed items.
+	 *
+	 * Called from 'wprss_items_insert_post'
 	 *
 	 * @since 2.3
 	 */
@@ -211,4 +287,52 @@
 		update_post_meta( $inserted_ID, 'wprss_item_date', $item->get_date( 'U' ) ); // Save as Unix timestamp format
 		update_post_meta( $inserted_ID, 'wprss_feed_id', $feed_ID);
 		do_action( 'wprss_items_create_post_meta', $inserted_ID, $item, $feed_ID );
+	}
+
+
+
+
+
+
+
+	/**
+	 * Fetches all feed items from all feed sources.
+	 * Iteratively calls 'wprss_fetch_insert_single_feed_items' for all feed sources.
+	 *
+	 * This function is used by the cron job or the debugging functions to get all feeds from all feed sources
+	 *
+	 * @param $all  If set to TRUE, the function will pull from all feed sources, regardless of their individual
+	 *              update interval. If set to FALSE, only feed sources using the global update system will be updated.
+	 *              (Optional) Default: TRUE.
+	 * @since 3.0
+	 */
+	function wprss_fetch_insert_all_feed_items( $all = TRUE ) {
+		// Get all feed sources
+		$feed_sources = wprss_get_all_feed_sources();
+
+		if( $feed_sources->have_posts() ) {
+			// Start by getting one feed source, we will cycle through them one by one,
+			// fetching feed items and adding them to the database in each pass
+			while ( $feed_sources->have_posts() ) {
+				$feed_sources->the_post();
+
+				$interval = get_post_meta( get_the_ID(), 'wprss_update_interval', TRUE );
+				$using_global_interval = ( $interval === wprss_get_default_feed_source_update_interval() || $interval === '' );
+
+				// Check if fetching from all, or if feed source uses the global interval
+				if ( $all === TRUE || $using_global_interval ) {
+					wp_schedule_single_event( time(), 'wprss_fetch_single_feed_hook', array( get_the_ID() ) );
+				}
+			}
+			wp_reset_postdata(); // Restore the $post global to the current post in the main query
+		}
+	}
+
+	/**
+	 * Runs the above function with parameter FALSE
+	 * 
+	 * @since 3.9
+	 */
+	function wprss_fetch_insert_all_feed_items_from_cron() {
+		wprss_fetch_insert_all_feed_items( FALSE );
 	}
