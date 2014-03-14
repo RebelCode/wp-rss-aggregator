33dbd9e2f8a238aa9c46d99ec153f2fa5f4d0baf
Miguel Muscat <miguelmuscat93@gmail.com>
2014-03-14 16:56:01 +0100

    Increased SimplePie timeout to 30s

--- includes/feed-importing.php
+++ includes/feed-importing.php
@@ -181,6 +181,7 @@
 
 		$feed->set_feed_url( $url );
 		$feed->force_feed( TRUE );
+		$feed->set_timeout( 30 );
 
 		//$feed->set_cache_duration( apply_filters( 'wp_feed_cache_transient_lifetime', 12 * HOUR_IN_SECONDS, $url ) );
 		$feed->enable_cache( FALSE );
