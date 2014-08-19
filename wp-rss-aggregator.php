b35aae3fbb25646911965cd0a0baf936622dd7e0
Miguel Muscat <miguelmuscat93@gmail.com>
2014-08-19 10:50:07 +0200

    Removed presstrends code

--- wp-rss-aggregator.php
+++ wp-rss-aggregator.php
@@ -416,69 +416,6 @@
     function wprss_load_textdomain() { 
         load_plugin_textdomain( 'wprss', false, WPRSS_LANG );
     }
-    
-    
-    // PressTrends WordPress Action
-    add_action( 'admin_init', 'wprss_presstrends_plugin' );  
-    /**
-     * Track plugin usage using PressTrends
-     * 
-     * @since  3.5
-     * @return void     
-     */  
-    function wprss_presstrends_plugin() {
-        $settings = get_option( 'wprss_settings_general' );
-        if ( ! isset( $settings['tracking'] ) || $settings['tracking'] != 1 ) return;
-
-        // PressTrends Account API Key
-        $api_key = 'znggu7vk7x2ddsiigkerzsca9q22xu1j53hp';
-        $auth    = 'd8giw5yyux4noasmo8gua98n7fv2hrl11';
-        // Start of Metrics
-        global $wpdb;
-        $data = get_transient( 'presstrends_cache_data' );
-        if ( !$data || $data == '' ) {
-            $api_base = 'http://api.presstrends.io/index.php/api/pluginsites/update?auth=';
-            $url      = $api_base . $auth . '&api=' . $api_key . '';
-            $count_posts    = wp_count_posts();
-            $count_pages    = wp_count_posts( 'page' );
-            $comments_count = wp_count_comments();
-            if ( function_exists( 'wp_get_theme' ) ) {
-                $theme_data = wp_get_theme();
-                $theme_name = urlencode( $theme_data->Name );
-            } else {
-                $theme_data = get_theme_data( get_stylesheet_directory() . '/style.css' );
-                $theme_name = $theme_data['Name'];
-            }
-            $plugin_name = '&';
-            foreach ( get_plugins() as $plugin_info ) {
-                $plugin_name .= $plugin_info['Name'] . '&';
-            }
-            // CHANGE __FILE__ PATH IF LOCATED OUTSIDE MAIN PLUGIN FILE
-            $plugin_data         = get_plugin_data( __FILE__ );
-            $posts_with_comments = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->posts WHERE post_type='post' AND comment_count > 0" );
-            $data                = array(
-                'url'             => base64_encode(site_url()),
-                'posts'           => $count_posts->publish,
-                'pages'           => $count_pages->publish,
-                'comments'        => $comments_count->total_comments,
-                'approved'        => $comments_count->approved,
-                'spam'            => $comments_count->spam,
-                'pingbacks'       => $wpdb->get_var( "SELECT COUNT(comment_ID) FROM $wpdb->comments WHERE comment_type = 'pingback'" ),
-                'post_conversion' => ( $count_posts->publish > 0 && $posts_with_comments > 0 ) ? number_format( ( $posts_with_comments / $count_posts->publish ) * 100, 0, '.', '' ) : 0,
-                'theme_version'   => $plugin_data['Version'],
-                'theme_name'      => $theme_name,
-                'site_name'       => str_replace( ' ', '', get_bloginfo( 'name' ) ),
-                'plugins'         => count( get_option( 'active_plugins' ) ),
-                'plugin'          => urlencode( $plugin_name ),
-                'wpversion'       => get_bloginfo( 'version' ),
-            );
-            foreach ( $data as $k => $v ) {
-                $url .= '&' . $k . '=' . $v . '';
-            }
-            wp_remote_get( $url );
-            set_transient( 'presstrends_cache_data', $data, 60 * 60 * 24 );
-            }
-        }  
 
 
 
