9848e23d9b238f7fea930cf44037133dbcec7191
Miguel Muscat <miguelmuscat93@gmail.com>
2014-08-23 15:14:04 +0200

    Added EDD licensing API functions

--- includes/licensing.php
+++ includes/licensing.php
@@ -1,5 +1,80 @@
 <?php
 
+
+/**
+ * Calls the EDD Software Licensing API to perform licensing tasks on the addon's store server.
+ *
+ * @since 4.4.5
+ */
+function wprss_edd_licensing_api( $addon, $license_key = NULL, $action = 'check_license' ) {
+	// If no license argument was given
+	if ( $license_key === NULL ) {
+		// Get the license key
+		$license_key = wprss_get_license_key( $addon );
+	}
+	// Get the license status from the DB
+	$license_status = wprss_get_license_status( $addon );
+
+	// Prepare constants
+	$item_name = strtoupper( $addon );
+	$item_name_constant = constant( "WPRSS_{$item_name}_SL_ITEM_NAME" );
+	$store_url_constant = constant( "WPRSS_{$item_name}_SL_STORE_URL" );
+
+	// data to send in our API request
+	$api_params = array(
+		'edd_action'	=> $action,
+		'license'		=> $license_key,
+		'item_name'		=> urlencode( $item_name_constant )
+	);
+
+	// Call the custom API.
+	$response = wp_remote_get( add_query_arg( $api_params, $store_url_constant ) );
+
+	// If the response is an error, return the value in the DB
+	if ( is_wp_error( $response ) ) return $license_status;
+
+	// decode the license data
+	$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+
+	// Update the DB option
+	$license_statuses["{$addon}_license_status"] = $license_data->license;
+	update_option( 'wprss_settings_license_statuses', $license_statuses );
+
+	// Return TRUE if it is 'active', FALSE otherwise
+	return $license_data->license;
+}
+
+
+/**
+ * Returns the license status. Also updates the status in the DB.
+ *
+ * @since 4.4.5
+ */
+function wprss_edd_check_license( $addon, $license_key = NULL ) {
+	return wprss_edd_licensing_api( $addon, $license_key, 'check_license' );
+}
+
+
+/**
+ * Activates an addon's license.
+ *
+ * @since 4.4.5
+ */
+function wprss_edd_activate_license( $addon, $license_key = NULL ) {
+	return wprss_edd_licensing_api( $addon, $license_key, 'activate_license' );
+}
+
+
+/**
+ * Deactivates an addon's license.
+ *
+ * @since 4.4.5
+ */
+function wprss_edd_deactivate_license( $addon, $license_key = NULL ) {
+	return wprss_edd_licensing_api( $addon, $license_key, 'deactivate_license' );
+}
+
+
 /**
 * Returns an array of the default license settings. Used for plugin activation.
 *
