ddf8f24748ae5c45cab12d46337cf904be0dab94
Mark Deutsch <markpd@gmail.com>
2015-02-07 11:21:18 +0700

    Remove Save Settings button on Licenses page

--- js/admin-licensing.js
+++ js/admin-licensing.js
@@ -51,7 +51,11 @@ jQuery( document ).ready( function($) {
 
 	};
 
-	$('.button-activate-license').click(manage_license);
-	$('.button-deactivate-license').click(manage_license);
+	// This .js is only enqueued on our settings page, so just check the tab we're on.
+	if ( document.location.href.search('tab=licenses_settings') > 0 ) {
+		$('.button-activate-license').click(manage_license);
+		$('.button-deactivate-license').click(manage_license);
+		$('.submit').remove();
+	}
 
 });
