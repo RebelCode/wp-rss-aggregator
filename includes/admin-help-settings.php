618968686cd5229983c29f438f0ef625eb8d6cc9
Mark Deutsch <markpd@gmail.com>
2015-02-25 19:00:17 +0700

    Updated Core .po file

--- includes/admin-help-settings.php
+++ includes/admin-help-settings.php
@@ -10,7 +10,7 @@ if( class_exists('WPRSS_Help') ) {
          *  General Section
          * -----------------
          */ // Limit feed items by age
-            'limit-feed-items-by-age'   =>  'The maximum age allowed for feed items.
+            'limit-feed-items-by-age'   => __( 'The maximum age allowed for feed items.
                                             <hr/>
 
                                             Items already imported will be deleted if they eventually exceed this age limit.
@@ -18,9 +18,9 @@ if( class_exists('WPRSS_Help') ) {
                                             Also, items in the RSS feed that are already older than this age will not be imported at all.
                                             <hr/>
 
-                                            <em>Leave empty for no limit.</em>',
+                                            <em>Leave empty for no limit.</em>', WPRSS_TEXT_DOMAIN),
             // Limit feed items per feed
-            'limit-feed-items-imported' => 'The maximum number of imported items to keep stored, for feed sources that do not have their own limit.
+            'limit-feed-items-imported' => __('The maximum number of imported items to keep stored, for feed sources that do not have their own limit.
                                             <hr/>
 
                                             When new items are imported and the limit for a feed source is exceeded, the oldest feed items for that feed source will be deleted to make room for the new ones.
@@ -28,89 +28,89 @@ if( class_exists('WPRSS_Help') ) {
                                             If you already have items imported from this feed source, setting this option now may delete some of your items, in order to comply with the limit.
                                             <hr/>
                                             
-                                            <em>Use 0 or leave empty for no limit.</em>',
+                                            <em>Use 0 or leave empty for no limit.</em>', WPRSS_TEXT_DOMAIN),
             // Feed processing interval
-            'cron-interval'             => 'How frequently should the feed sources (that do not have their own update interval) check for updates and fetch items accordingly.
+            'cron-interval'             => __('How frequently should the feed sources (that do not have their own update interval) check for updates and fetch items accordingly.
 
-                                            It is recommended to not have more than 20 feed sources that use this global update interval. Having too many feed sources updating precisely at the same time can cause the WP Cron System to crash.',
+                                            It is recommended to not have more than 20 feed sources that use this global update interval. Having too many feed sources updating precisely at the same time can cause the WP Cron System to crash.', WPRSS_TEXT_DOMAIN),
             // Custom Feed URL
-            'custom-feed-url'           => 'The URL of the custom feed, located at <code>http://yoursite.com/[custom feed url]</code>.
+            'custom-feed-url'           => __('The URL of the custom feed, located at <code>http://yoursite.com/[custom feed url]</code>.
                                             <hr/>
 
                                             WP RSS Aggregator allows you to create a custom RSS feed, that contains all of your imported feed items. This setting allows you to change the URL of this custom feed.
 
                                             <hr/>
 
-                                            <strong>Note:</strong> You may be required to refresh you Permalinks after you change this setting, by going to <em>Settings <i class="fa fa-angle-right"></i> Permalinks</e> and clicking <em>Save</em>.',
+                                            <strong>Note:</strong> You may be required to refresh you Permalinks after you change this setting, by going to <em>Settings <i class="fa fa-angle-right"></i> Permalinks</e> and clicking <em>Save</em>.', WPRSS_TEXT_DOMAIN),
             // Custom Feed Title
-            'custom-feed-title'         => 'The title of the custom feed.
+            'custom-feed-title'         => __('The title of the custom feed.
 
-                                            This title will be included in the RSS source of the custom feed, in a <code>&lt;title&gt;</code> tag.',
+                                            This title will be included in the RSS source of the custom feed, in a <code>&lt;title&gt;</code> tag.', WPRSS_TEXT_DOMAIN),
             // Custom Feed Limit
-            'custom-feed-limit'         => 'The maximum number of feed items in the custom feed.',
+            'custom-feed-limit'         => __('The maximum number of feed items in the custom feed.', WPRSS_TEXT_DOMAIN),
 
         /* --------------------------
          *  General Display Settings
          * --------------------------
          */ // Link titles
-            'link-enable'               => 'Check this box to make the feed item titles link to the original article.',
+            'link-enable'               => __('Check this box to make the feed item titles link to the original article.', WPRSS_TEXT_DOMAIN),
             // Title Maximum length
-            'title-limit'               => 'Set the maximum number of characters to show for feed item titles.
+            'title-limit'               => __('Set the maximum number of characters to show for feed item titles.
                                             <hr/>
 
-                                            <em>Leave empty for no limit.</em>',
+                                            <em>Leave empty for no limit.</em>', WPRSS_TEXT_DOMAIN),
             // Show Authors
-            'authors-enable'            => 'Check this box to show the author for each feed item, if it is available.',
+            'authors-enable'            => __('Check this box to show the author for each feed item, if it is available.', WPRSS_TEXT_DOMAIN),
             // Video Links
-            'video-links'               => 'For feed items from YouTube, Vimeo or Dailymotion, you can choose whether you want to have the items link to the original page link, or a link to the embedded video player only.',
+            'video-links'               => __('For feed items from YouTube, Vimeo or Dailymotion, you can choose whether you want to have the items link to the original page link, or a link to the embedded video player only.', WPRSS_TEXT_DOMAIN),
             // Pagination Type
-            'pagination'                => 'The type of pagination to use when showing feed items on multiple pages.
+            'pagination'                => __('The type of pagination to use when showing feed items on multiple pages.
 
                                             The first shows two links, "Older" and "Newer", which allow you to navigate through the pages.
 
-                                            The second shows links for all the pages, together with links for the next and previous pages.',
+                                            The second shows links for all the pages, together with links for the next and previous pages.', WPRSS_TEXT_DOMAIN),
             // Feed Limit
-            'feed-limit'                => 'The maximum number of feed items to display when using the shortcode.
+            'feed-limit'                => __('The maximum number of feed items to display when using the shortcode.
 
-                                            This enables pagination if set to a number smaller than the number of items to be displayed.',
+                                            This enables pagination if set to a number smaller than the number of items to be displayed.', WPRSS_TEXT_DOMAIN),
             // Open Links Behaviour
-            'open-dd'                   => 'Choose how you want links to be opened. This applies to the feed item title and the source link.',
+            'open-dd'                   => __('Choose how you want links to be opened. This applies to the feed item title and the source link.', WPRSS_TEXT_DOMAIN),
             // Set links as no follow
-            'follow-dd'                 => 'Enable this option to set all links displayed as "NoFollow".
+            'follow-dd'                 => __('Enable this option to set all links displayed as "NoFollow".
                                             <hr/>
 
-                                            "Nofollow" provides a way to tell search engines to <em>not</em> follow certain links, such as links to feed items in this case.',
+                                            "Nofollow" provides a way to tell search engines to <em>not</em> follow certain links, such as links to feed items in this case.', WPRSS_TEXT_DOMAIN),
 
         /* -------------------------
          *  Source Display Settings
          * -------------------------
          */ // Source Enabled
-            'source-enable'             => 'Enable this option to show the feed source name for each feed item.',
+            'source-enable'             => __('Enable this option to show the feed source name for each feed item.', WPRSS_TEXT_DOMAIN),
             // Text preceding source
-            'text-preceding-source'     => 'Enter the text that you want to show before the source name. A space is automatically added between this text and the feed source name.',
+            'text-preceding-source'     => __('Enter the text that you want to show before the source name. A space is automatically added between this text and the feed source name.', WPRSS_TEXT_DOMAIN),
             // Source Link
-            'source-link'               => 'Enable this option to link the feed source name to the RSS feed\'s source site.',
+            'source-link'               => __('Enable this option to link the feed source name to the RSS feed\'s source site.', WPRSS_TEXT_DOMAIN),
 
         /* -------------------------
          *  Date Display Settings
          * -------------------------
          */ // Source Enabled
-            'date-enable'               => 'Enable this to show the feed item\'s date.',
+            'date-enable'               => __('Enable this to show the feed item\'s date.', WPRSS_TEXT_DOMAIN),
             // Text preceding date
-            'text-preceding-date'       => 'Enter the text that you want to show before the feed item date. A space is automatically added between this text and the date.',
+            'text-preceding-date'       => __('Enter the text that you want to show before the feed item date. A space is automatically added between this text and the date.', WPRSS_TEXT_DOMAIN),
             // Date Format
-            'date-format'               => 'The format to use for the feed item dates, as a PHP date format.',
+            'date-format'               => __('The format to use for the feed item dates, as a PHP date format.', WPRSS_TEXT_DOMAIN),
             // Time Ago Format Enable
-            'time-ago-format-enable'    => 'Enable this option to show the elapsed time from the feed item\'s date and time to the present time.
-                                            <em>Eg. 2 hours ago</em>',
+            'time-ago-format-enable'    => __('Enable this option to show the elapsed time from the feed item\'s date and time to the present time.
+                                            <em>Eg. 2 hours ago</em>', WPRSS_TEXT_DOMAIN),
 
         /* --------
          *  Styles
          * --------
          */ // Styles Disable
-            'styles-disable'            => 'Check this box to disable all plugin styles used for displaying feed items.
+            'styles-disable'            => __('Check this box to disable all plugin styles used for displaying feed items.
 
-                                            This will allow you to provide your own custom CSS styles for displaying the feed items.'
+                                            This will allow you to provide your own custom CSS styles for displaying the feed items.', WPRSS_TEXT_DOMAIN)
     );
     $help->add_tooltips( $tooltips, $prefix );
 }
