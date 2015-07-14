941d45a23f26ee357e91a71351840075854756e1
Anton Ukhanev <xedin.unknown@gmail.com>
2015-07-14 19:36:01 +0200

    Ammended tooltip text to exclude space indentation.
    This is necessary for compatibility with gettext translation software.

--- includes/admin-help-metaboxes.php
+++ includes/admin-help-metaboxes.php
@@ -16,64 +16,64 @@
 				// Feed Source URL
 				'wprss_url'             => __('The URL of the feed source. In most cases, the URL of the site will also work, but for best results we recommend trying to find the URL of the RSS feed.
 
-										Also include the <code>http://</code> prefix in the URL.', WPRSS_TEXT_DOMAIN),
+' .											'Also include the <code>http://</code> prefix in the URL.', WPRSS_TEXT_DOMAIN),
 				// Feed limit
 				'wprss_limit'           => __('The maximum number of imported items from this feed to keep stored.
 
-					When new items are imported and the limit is exceeded, the oldest feed items will be deleted to make room for new ones.
+'.											'When new items are imported and the limit is exceeded, the oldest feed items will be deleted to make room for new ones.
 
-					If you already have items imported from this feed source, setting this option now may delete some of your items, in order to comply with the limit.', WPRSS_TEXT_DOMAIN),
+'.											'If you already have items imported from this feed source, setting this option now may delete some of your items, in order to comply with the limit.', WPRSS_TEXT_DOMAIN),
 				// Link to Enclosure
 				'wprss_enclosure'       => __('Tick this box to make feed items link to the URL in the enclosure tag, rather than link to the original article.
 
-										Enclosure tags are RSS tags that may be included with a feed items. These tags typically contain links to images, audio, videos, attachment files or even flash content.
+'.											'Enclosure tags are RSS tags that may be included with a feed items. These tags typically contain links to images, audio, videos, attachment files or even flash content.
 
-										If you are not sure leave this setting blank.', WPRSS_TEXT_DOMAIN),
+'.											'If you are not sure leave this setting blank.', WPRSS_TEXT_DOMAIN),
 
 				'wprss_unique_titles'   => __('Whether to allow multiple feed items to have the same title. When checked, if a feed item has the same title as a previously-imported feed item, it will not be imported.
 
-									   This can be useful in cases where permalinks change, or where multiple permalinks refer to the same item.', WPRSS_TEXT_DOMAIN),
+'.											'This can be useful in cases where permalinks change, or where multiple permalinks refer to the same item.', WPRSS_TEXT_DOMAIN),
 
 
-			/* -------------------------
-			 *  Feed Processing Metabox
-			 * -------------------------
-			 */
+				/* -------------------------
+				 *  Feed Processing Metabox
+				 * -------------------------
+				 */
 				// Feed State
 				'wprss_state'           => __('State of the feed, active or paused.
 
-										If active, the feed source will fetch items periodically, according to the settings below.
+'.											'If active, the feed source will fetch items periodically, according to the settings below.
 
-										If paused, the feed source will not fetch feed items periodically.', WPRSS_TEXT_DOMAIN),
+'.											'If paused, the feed source will not fetch feed items periodically.', WPRSS_TEXT_DOMAIN),
 				// Activate Feed: [date]
 				'wprss_activate_feed'   => __('You can set a time, in UTC, in the future when the feed source will become active, if it is paused.
 
-										Leave blank to activate immediately.', WPRSS_TEXT_DOMAIN),
+'.											'Leave blank to activate immediately.', WPRSS_TEXT_DOMAIN),
 				// Pause Feed: [date]
 				'wprss_pause_feed'      => __('You can set a time, in UTC, in the future when the feed source will become paused, if it is active.
 
-										Leave blank to never pause.', WPRSS_TEXT_DOMAIN),
+'.											'Leave blank to never pause.', WPRSS_TEXT_DOMAIN),
 				// Update Interval
 				'wprss_update_interval' => __('How frequently the feed source should check for new items and fetch if needed.
 
-										If left as <em>Default</em>, the interval in the global settings is used.', WPRSS_TEXT_DOMAIN),
+'.											'If left as <em>Default</em>, the interval in the global settings is used.', WPRSS_TEXT_DOMAIN),
 				// Delete items older than: [date]
 				'wprss_age_limit'       => __('The maximum age allowed for feed items. Very useful if you are only concerned with, say, last week\'s news.
 
-										Items already imported will be deleted if they eventually exceed this age limit.
+'.											'Items already imported will be deleted if they eventually exceed this age limit.
 
-										Also, items in the RSS feed that are already older than this age will not be imported at all.
+'.											'Also, items in the RSS feed that are already older than this age will not be imported at all.
 
-										Leaving empty to use the <em>Limit feed items by age</em> option in the general settings.', WPRSS_TEXT_DOMAIN),
+'.											'Leaving empty to use the <em>Limit feed items by age</em> option in the general settings.', WPRSS_TEXT_DOMAIN),
 
-			/* ----------------------
-			 *  Feed Preview Metabox
-			 * ----------------------
-			 */
-			// Force Feed
-			'wprss_force_feed'      => __('Use this option if you are seeing an <q>Invalid feed URL</q> error in the Feed Preview above, but you are sure that the URL is correct.
+				/* ----------------------
+				 *  Feed Preview Metabox
+				 * ----------------------
+				 */
+				// Force Feed
+				'wprss_force_feed'      => __('Use this option if you are seeing an <q>Invalid feed URL</q> error in the Feed Preview above, but you are sure that the URL is correct.
 
-									Note, however, that this will disable auto-discovery, meaning that the given URL must be an RSS feed URL. Using the site\'s URL will not work.', WPRSS_TEXT_DOMAIN)
+'.											'Note, however, that this will disable auto-discovery, meaning that the given URL must be an RSS feed URL. Using the site\'s URL will not work.', WPRSS_TEXT_DOMAIN)
 		);
 		$help->add_tooltips( $tooltips, $prefix );
 	}
--- includes/admin-help-settings.php
+++ includes/admin-help-settings.php
@@ -11,47 +11,47 @@ if( class_exists('WPRSS_Help') ) {
          * -----------------
          */ // Limit feed items by age
             'limit-feed-items-by-age'   => __( 'The maximum age allowed for feed items.
-                                            <hr/>
+'.											'<hr/>
 
-                                            Items already imported will be deleted if they eventually exceed this age limit.
+'.											'Items already imported will be deleted if they eventually exceed this age limit.
 
-                                            Also, items in the RSS feed that are already older than this age will not be imported at all.
-                                            <hr/>
+'.											'Also, items in the RSS feed that are already older than this age will not be imported at all.
+'.											'<hr/>
 
-                                            <em>Leave empty for no limit.</em>', WPRSS_TEXT_DOMAIN),
+'.											'<em>Leave empty for no limit.</em>', WPRSS_TEXT_DOMAIN),
             // Limit feed items per feed
             'limit-feed-items-imported' => __('The maximum number of imported items to keep stored, for feed sources that do not have their own limit.
-                                            <hr/>
+'.											'<hr/>
 
-                                            When new items are imported and the limit for a feed source is exceeded, the oldest feed items for that feed source will be deleted to make room for the new ones.
+'.											'When new items are imported and the limit for a feed source is exceeded, the oldest feed items for that feed source will be deleted to make room for the new ones.
 
-                                            If you already have items imported from this feed source, setting this option now may delete some of your items, in order to comply with the limit.
-                                            <hr/>
-                                            
-                                            <em>Use 0 or leave empty for no limit.</em>', WPRSS_TEXT_DOMAIN),
+'.											'If you already have items imported from this feed source, setting this option now may delete some of your items, in order to comply with the limit.
+'.											'<hr/>
+
+'.											'<em>Use 0 or leave empty for no limit.</em>', WPRSS_TEXT_DOMAIN),
             // Feed processing interval
             'cron-interval'             => __('How frequently should the feed sources (that do not have their own update interval) check for updates and fetch items accordingly.
 
-                                            It is recommended to not have more than 20 feed sources that use this global update interval. Having too many feed sources updating precisely at the same time can cause the WP Cron System to crash.', WPRSS_TEXT_DOMAIN),
+'.											'It is recommended to not have more than 20 feed sources that use this global update interval. Having too many feed sources updating precisely at the same time can cause the WP Cron System to crash.', WPRSS_TEXT_DOMAIN),
             // Unique titles only
             'unique-titles'             => __('Whether to allow multiple feed items to have the same title. When checked, if a feed item has the same title as a previously-imported feed item from any feed source, it will not be imported.
 
-                                            This can be useful in cases where permalinks change, or where multiple permalinks refer to the same item.
+'.											'This can be useful in cases where permalinks change, or where multiple permalinks refer to the same item.
 
-                                            Since this feature requires checking every post title, WordPress installs with a significant amount of posts may notice a slight slowdown of the post import process.', WPRSS_TEXT_DOMAIN),
+'.											'Since this feature requires checking every post title, WordPress installs with a significant amount of posts may notice a slight slowdown of the post import process.', WPRSS_TEXT_DOMAIN),
             // Custom Feed URL
             'custom-feed-url'           => __('The URL of the custom feed, located at <code>http://yoursite.com/[custom feed url]</code>.
-                                            <hr/>
+'.											'<hr/>
 
-                                            WP RSS Aggregator allows you to create a custom RSS feed, that contains all of your imported feed items. This setting allows you to change the URL of this custom feed.
+'.											'WP RSS Aggregator allows you to create a custom RSS feed, that contains all of your imported feed items. This setting allows you to change the URL of this custom feed.
 
-                                            <hr/>
+'.											'<hr/>
 
-                                            <strong>Note:</strong> You may be required to refresh you Permalinks after you change this setting, by going to <em>Settings <i class="fa fa-angle-right"></i> Permalinks</e> and clicking <em>Save</em>.', WPRSS_TEXT_DOMAIN),
+'.											'<strong>Note:</strong> You may be required to refresh you Permalinks after you change this setting, by going to <em>Settings <i class="fa fa-angle-right"></i> Permalinks</e> and clicking <em>Save</em>.', WPRSS_TEXT_DOMAIN),
             // Custom Feed Title
             'custom-feed-title'         => __('The title of the custom feed.
 
-                                            This title will be included in the RSS source of the custom feed, in a <code>&lt;title&gt;</code> tag.', WPRSS_TEXT_DOMAIN),
+'.											'This title will be included in the RSS source of the custom feed, in a <code>&lt;title&gt;</code> tag.', WPRSS_TEXT_DOMAIN),
             // Custom Feed Limit
             'custom-feed-limit'         => __('The maximum number of feed items in the custom feed.', WPRSS_TEXT_DOMAIN),
 
@@ -62,9 +62,9 @@ if( class_exists('WPRSS_Help') ) {
             'link-enable'               => __('Check this box to make the feed item titles link to the original article.', WPRSS_TEXT_DOMAIN),
             // Title Maximum length
             'title-limit'               => __('Set the maximum number of characters to show for feed item titles.
-                                            <hr/>
+'.											'<hr/>
 
-                                            <em>Leave empty for no limit.</em>', WPRSS_TEXT_DOMAIN),
+'.											'<em>Leave empty for no limit.</em>', WPRSS_TEXT_DOMAIN),
             // Show Authors
             'authors-enable'            => __('Check this box to show the author for each feed item, if it is available.', WPRSS_TEXT_DOMAIN),
             // Video Links
@@ -72,20 +72,20 @@ if( class_exists('WPRSS_Help') ) {
             // Pagination Type
             'pagination'                => __('The type of pagination to use when showing feed items on multiple pages.
 
-                                            The first shows two links, "Older" and "Newer", which allow you to navigate through the pages.
+'.											'The first shows two links, "Older" and "Newer", which allow you to navigate through the pages.
 
-                                            The second shows links for all the pages, together with links for the next and previous pages.', WPRSS_TEXT_DOMAIN),
+'.											'The second shows links for all the pages, together with links for the next and previous pages.', WPRSS_TEXT_DOMAIN),
             // Feed Limit
             'feed-limit'                => __('The maximum number of feed items to display when using the shortcode.
 
-                                            This enables pagination if set to a number smaller than the number of items to be displayed.', WPRSS_TEXT_DOMAIN),
+'.											'This enables pagination if set to a number smaller than the number of items to be displayed.', WPRSS_TEXT_DOMAIN),
             // Open Links Behaviour
             'open-dd'                   => __('Choose how you want links to be opened. This applies to the feed item title and the source link.', WPRSS_TEXT_DOMAIN),
             // Set links as no follow
             'follow-dd'                 => __('Enable this option to set all links displayed as "NoFollow".
-                                            <hr/>
+'.											'<hr/>
 
-                                            "Nofollow" provides a way to tell search engines to <em>not</em> follow certain links, such as links to feed items in this case.', WPRSS_TEXT_DOMAIN),
+'.											'"Nofollow" provides a way to tell search engines to <em>not</em> follow certain links, such as links to feed items in this case.', WPRSS_TEXT_DOMAIN),
 
         /* -------------------------
          *  Source Display Settings
@@ -108,7 +108,7 @@ if( class_exists('WPRSS_Help') ) {
             'date-format'               => __('The format to use for the feed item dates, as a PHP date format.', WPRSS_TEXT_DOMAIN),
             // Time Ago Format Enable
             'time-ago-format-enable'    => __('Enable this option to show the elapsed time from the feed item\'s date and time to the present time.
-                                            <em>Eg. 2 hours ago</em>', WPRSS_TEXT_DOMAIN),
+'.											'<em>Eg. 2 hours ago</em>', WPRSS_TEXT_DOMAIN),
 
         /* --------
          *  Styles
@@ -116,7 +116,7 @@ if( class_exists('WPRSS_Help') ) {
          */ // Styles Disable
             'styles-disable'            => __('Check this box to disable all plugin styles used for displaying feed items.
 
-                                            This will allow you to provide your own custom CSS styles for displaying the feed items.', WPRSS_TEXT_DOMAIN),
+'.											'This will allow you to provide your own custom CSS styles for displaying the feed items.', WPRSS_TEXT_DOMAIN),
 		
 		/*
 		 * -------
@@ -125,12 +125,11 @@ if( class_exists('WPRSS_Help') ) {
 		 */ // Certificate Path
 			'certificate-path'			=> __( 'Path to the file containing one or more certificates.
 
-											These will be used to verify certificates over secure connection, such as when fetching a remote resource over HTTPS.
-											
-											Relative path will be relative to the WordPress root.
-											
-											<strong>Default:</strong> path to certificate file bundled with WordPress.'
-				, WPRSS_TEXT_DOMAIN )
+'.											'These will be used to verify certificates over secure connection, such as when fetching a remote resource over HTTPS.
+
+'.											'Relative path will be relative to the WordPress root.
+
+'.											'<strong>Default:</strong> path to certificate file bundled with WordPress.', WPRSS_TEXT_DOMAIN )
 		
     );
     $help->add_tooltips( $tooltips, $prefix );
