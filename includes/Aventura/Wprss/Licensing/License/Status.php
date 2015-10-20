1184f8cd41472fe03e90d62ff218e7ad94bf7929
Miguel Muscat <miguelmuscat93@gmail.com>
2015-10-20 17:29:04 +0200

    License Status Enum class (WPRACORE-278)
    Enum class for license statuses.

---
+++ includes/Aventura/Wprss/Licensing/License/Status.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Aventura\Wprss\Licensing\License;
+
+/**
+ * Enum-style abstract class for license statuses.
+ */
+abstract class Status {
+	const VALID		=	'valid';
+	const INVALID	=	'invalid';
+	const EXPIRED	=	'expired';
+}
