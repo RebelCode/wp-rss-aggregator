612c7d43bea4947aca44d39a9192671932d9e697
Miguel Muscat <miguelmuscat93@gmail.com>
2015-11-18 12:20:14 +0100

    Fixed namespace for ImageCache Image class
    - Namespace `Aventura\Wprss\Core\Caching\ImageCaching` renamed to `ImageCache`

--- includes/Aventura/Wprss/Core/Caching/ImageCache/Image.php
+++ includes/Aventura/Wprss/Core/Caching/ImageCache/Image.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Aventura\Wprss\Core\Caching\ImageCaching;
+namespace Aventura\Wprss\Core\Caching\ImageCache;
 
 if (!class_exists('\\WPRSS_Image_Cache_Image')) {
 	require WPRSS_INC . 'image-caching.php';
 }
 
 /**
- * Image class for ImageCaching module.
+ * Image class for ImageCache module.
  */
 class Image extends \WPRSS_Image_Cache_Image {
 
