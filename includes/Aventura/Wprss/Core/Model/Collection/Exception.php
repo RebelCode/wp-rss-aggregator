940564b8b702c8a5a547c2b0a66dbe816d75e598
Anton Ukhanev <xedin.unknown@gmail.com>
2016-08-13 15:26:08 +0200

    Added `Collection\Exception` (WPRACORE-433)
    This starts a new `Collection` model family.

---
+++ includes/Aventura/Wprss/Core/Model/Collection/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Aventura\Wprss\Core\Model\Collection;
+
+use Aventura\Wprss\Core;
+
+/**
+ * A generic collection exception.
+ *
+ * @since [*next-version*]
+ */
+class Exception extends Core\Plugin\Exception
+{
+}
