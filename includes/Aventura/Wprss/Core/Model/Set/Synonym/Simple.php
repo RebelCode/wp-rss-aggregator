327de9defa7bd2fc9b707c37ee389d0fad0a521a
Anton Ukhanev <xedin.unknown@gmail.com>
2016-11-18 11:56:45 +0100

    Added synonym set classes (WPRACORE-433)

---
+++ includes/Aventura/Wprss/Core/Model/Set/Synonym/Simple.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Aventura\Wprss\Core\Model\Set\Synonym;
+
+/**
+ * A simple set of synonyms.
+ *
+ * @since [*next-version*]
+ */
+class Simple extends AbstractGenericSynonymSet implements SynonymSetInterface
+{
+}
