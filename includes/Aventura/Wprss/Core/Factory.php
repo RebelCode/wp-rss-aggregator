3aa774b301f6e2ea90061668ee802f7d4e044707
Anton Ukhanev <xedin.unknown@gmail.com>
2017-02-14 16:41:34 +0100

    Plugin `Factory` now uses DI (WPRACORE-492)
    This class is deprecated, but preserved for BC.

--- includes/Aventura/Wprss/Core/Factory.php
+++ includes/Aventura/Wprss/Core/Factory.php
@@ -2,22 +2,61 @@
 
 namespace Aventura\Wprss\Core;
 
+use Dhii\Di\FactoryInterface;
+
 /**
+ * @deprecated [*next-version*] Here only for BC.
  * @since 4.8.1
  */
 class Factory extends Plugin\FactoryAbstract
 {
+    /**
+     * Creates the plugin instance.
+     *
+     * @since 4.8.1
+     * @param array $data Data for the plugin.
+     * @return \Aventura\Wprss\Core\Plugin
+     */
     public function _create($data = array())
     {
-        $plugin = new Plugin($data);
-        $factory = new ComponentFactory($plugin);
-        $plugin->setFactory($factory);
-
-        $plugin->setLogger($factory->createLogger());
-        $plugin->setEventManager($factory->createEventManager());
-
-        $plugin->hook();
+        $plugin = $this->_getFactory()->make($this->_p('plugin'), $data);
 
         return $plugin;
+    }
+
+    /**
+     * Prefixes a service name with the WPRA service ID prefix.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $name A service name.
+     * @return string The prefixed name.
+     */
+    protected function _p($name)
+    {
+        return \WPRSS_SERVICE_ID_PREFIX . $name;
+    }
+
+    /**
+     * Gets service the factory.
+     *
+     * @since [*next-version*]
+     *
+     * @return FactoryInterface The factory.
+     */
+    protected function _getFactory()
+    {
+        return $this->_getContainer()->get($this->_p('factory'));
+    }
+
+    /**
+     * Retrieve the DI container.
+     *
+     * @since [*next-version*]
+     * @return ContainerInterface The container instance.
+     */
+    protected function _getContainer()
+    {
+        return wprss_wp_container();
     }
 }
