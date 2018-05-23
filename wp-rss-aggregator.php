b1630e907d9146cb4e2f4203472aa9d05a98c6cb
Anton Ukhanev <xedin.unknown@gmail.com>
2018-05-23 09:50:23 +0200

    Updated version in plugin header

--- wp-rss-aggregator.php
+++ wp-rss-aggregator.php
@@ -3,7 +3,7 @@
      * Plugin Name: WP RSS Aggregator
      * Plugin URI: https://www.wprssaggregator.com/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpraplugin
      * Description: Imports and aggregates multiple RSS Feeds.
-     * Version: 4.11.2
+     * Version: 4.11.3
      * Author: RebelCode
      * Author URI: https://www.wprssaggregator.com
      * Text Domain: wprss
