c78ce7ebb0956a460bcbe6e0eb967b19768b8c03
Miguel Muscat <miguelmuscat93@gmail.com>
2019-01-14 18:12:20 +0100

    Added utility AJAX functions

--- includes/admin-intro.php
+++ includes/admin-intro.php
@@ -27,3 +27,41 @@ function wprss_set_intro_step($step)
 {
     update_option(WPRSS_INTRO_STEP_OPTION, max($step, 0));
 }
+
+/**
+ * Sends an AJAX success response.
+ *
+ * @since [*next-version*]
+ *
+ * @param array $data   Optional data to send.
+ * @param int   $status Optional HTTP status code of the response.
+ */
+function wprss_ajax_success_response($data = [], $status = 200)
+{
+    echo json_encode([
+        'success' => true,
+        'error' => '',
+        'data' => $data,
+        'status' => $status,
+    ]);
+    wp_die();
+}
+
+/**
+ * Sends an AJAX success response.
+ *
+ * @since [*next-version*]
+ *
+ * @param string $message Optional error message.
+ * @param int    $status  Optional HTTP status code of the response.
+ */
+function wprss_ajax_error_response($message, $status = 400)
+{
+    echo json_encode([
+        'success' => false,
+        'error' => $message,
+        'data' => [],
+        'status' => $status,
+    ]);
+    wp_die();
+}
