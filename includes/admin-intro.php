f2bc0bbd405be3c1116b7a83b25a975f755534b1
Miguel Muscat <miguelmuscat93@gmail.com>
2019-01-14 18:10:48 +0100

    Added functions to get or set the intro step
    WPRACORE-561

--- includes/admin-intro.php
+++ includes/admin-intro.php
@@ -3,3 +3,27 @@
 if (!defined('ABSPATH')) {
     die;
 }
+
+/**
+ * Retrieves the step the user has reached in the introduction.
+ *
+ * @since [*next-version*]
+ *
+ * @return int
+ */
+function wprss_get_intro_step()
+{
+    return get_option(WPRSS_INTRO_STEP_OPTION, 0);
+}
+
+/**
+ * Sets the step the user has reached in the introduction.
+ *
+ * @since [*next-version*]
+ *
+ * @param int $step A positive integer.
+ */
+function wprss_set_intro_step($step)
+{
+    update_option(WPRSS_INTRO_STEP_OPTION, max($step, 0));
+}
