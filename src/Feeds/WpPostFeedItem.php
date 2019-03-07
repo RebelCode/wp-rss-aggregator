b4a33ae759b38d137166a1f0f24abd37e618eb45
Miguel Muscat <miguelmuscat93@gmail.com>
2019-03-07 17:50:10 +0100

    Readded code that got lost in a merge
    WPRACORE-557

--- src/Feeds/WpPostFeedItem.php
+++ src/Feeds/WpPostFeedItem.php
@@ -2,6 +2,8 @@
 
 namespace RebelCode\Wpra\Core\Feeds;
 
+use RebelCode\Wpra\Core\Data\ArrayDataSet;
+use RebelCode\Wpra\Core\Data\MergedDataSet;
 use RebelCode\Wpra\Core\Data\WpCptDataSet;
 use WP_Post;
 
@@ -34,5 +36,25 @@ class WpPostFeedItem extends WpCptDataSet
             'author' => 'item_author',
             'enclosure' => 'item_enclosure',
         ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    protected function createInnerDataSet(WP_Post $post, $metaPrefix = '', $aliases = [])
+    {
+        // Create the parent data set
+        $parent = parent::createInnerDataSet($post, $metaPrefix, $aliases);
+        // Create the feed source data set model
+        $feedId = $parent['source_id'];
+        $source = new WpPostFeedSource($feedId);
+        // Merge the parent with an array data set that contains the "source"
+        $inner = new MergedDataSet(
+            new ArrayDataSet(['source' => $source,]),
+            parent::createInnerDataSet($post, $metaPrefix, $aliases)
+        );
+        return $inner;
     }
 }
