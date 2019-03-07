0604814b12040fef09cb4177ffb139b2fef8b604
Miguel Muscat <miguelmuscat93@gmail.com>
2019-03-07 18:07:15 +0100

    Added empty admin templates page and menu item
    WPRACORE-557

--- includes/admin-templates.php
+++ includes/admin-templates.php
@@ -53,3 +53,23 @@ add_action('init', function () {
     register_post_type(WPRSS_FEED_TEMPLATE_CPT, $args);
 });
 
+// Adds the "Templates" page and menu item
+add_action('admin_menu', function () {
+    add_submenu_page(
+        'edit.php?post_type=wprss_feed',
+        __('Templates', 'wprss'),
+        __('Templates', 'wprss'),
+        'edit_feed_templates',
+        'wprss-feed-templates',
+        'wprss_render_admin_templates_page'
+    );
+}, 10);
+
+/**
+ * Renders the admin "Templates" page.
+ *
+ * @since [*next-version*]
+ */
+function wprss_render_admin_templates_page()
+{
+}
