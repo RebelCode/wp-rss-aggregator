ae0de8ba6962fc43c78c3b3022c7660f1a1ef54a
Roman Lytvynenko <litvinenko95@gmail.com>
2019-03-12 13:12:39 +0200

    templates table, simple version
    WPRACORE-577

--- js/src/templates/App.vue
+++ js/src/templates/App.vue
@@ -1,17 +1,68 @@
 <script>
+  import VueTable from 'vue-wp-list-table/dist/vue-wp-list-table.common'
   export default {
+    data () {
+      return {
+        columns: {
+          name: {
+            label: ('Template Name'),
+          },
+          style: {
+            label: ('Template Style'),
+          },
+          previewTemplate: {
+            label: ('Preview')
+          }
+        },
+
+        list: [{
+          id: 1,
+          title: 'Foo',
+          template_type: 'list',
+        }, {
+          id: 2,
+          title: 'Bar',
+          template_type: '__built_in',
+        }]
+      }
+    },
     inject: [
       'hooks'
     ],
     mounted () {
       console.info('Welcome, Neo', this, this.hooks)
     },
-    render (h) {
-      let content = h('div', [
-        h('div', {class: 'postbox'}, [
-          h('div', {class: 'inside'}, ['App.'])
-        ])
-      ])
+    render () {
+      let cells = this.hooks.apply('wpra-templates-list-cells', this, {
+        name: function ({ row }) {
+          return [
+            <div>{ row.title }</div>
+          ]
+        },
+        style: function ({ row }) {
+          return [
+            <div>{ row.template_type }</div>
+          ]
+        },
+        previewTemplate: function ({ row }) {
+          return [
+            <div>
+              <span class="dashicons dashicons-desktop"></span>
+            </div>
+          ]
+        },
+      })
+
+      let content = <div>
+        <VueTable
+          columns={this.columns}
+          rows={this.list}
+          scopedSlots={
+            cells
+          }
+        >
+        </VueTable>
+      </div>
       return this.hooks.apply('wpra-templates-list', this, content)
     }
   }
--- js/src/templates/app.js
+++ js/src/templates/app.js
@@ -12,6 +12,13 @@ export default {
   run ({ container }) {
     console.info(container.vue)
 
+    container.hooks.register('wpra-templates-list-cells', function (cells) {
+      cells['id'] = ({ row }) => {
+        return this.$createElement('div', ['ID: ' + row.id])
+      }
+      return cells
+    })
+
     container.hooks.register('wpra-templates-list', function (vnode) {
       vnode.children.push(this.$createElement('div', ['From hook.']))
       console.info({vnode, 'this': this})
