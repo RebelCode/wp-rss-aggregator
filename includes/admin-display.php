4d706403802311236dc82dd6eb730582c6d6adb1
Miguel Muscat <miguelmuscat93@gmail.com>
2019-03-14 10:11:41 +0100

    Fixed various admin role name mismatches

--- includes/admin-display.php
+++ includes/admin-display.php
@@ -468,7 +468,7 @@
             $id = $_POST['id'];
             $response->setAjaxData($kFeedSourceId, $id);
 
-            if (!current_user_can('edit_feed_sources')) {
+            if (!current_user_can('edit_feeds')) {
                 throw new Exception($wprss->__(array('Could not schedule fetch for source #%1$s: user must have sufficient priviledges', $id)));
             }
 
