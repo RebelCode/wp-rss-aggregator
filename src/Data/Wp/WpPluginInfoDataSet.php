e722bf1aea69f8f5d776781b2a12c48e6d7c0dcf
Miguel Muscat <miguelmuscat93@gmail.com>
2019-03-20 18:31:34 +0100

    Added dataset class for plugin info

---
+++ src/Data/Wp/WpPluginInfoDataSet.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace RebelCode\Wpra\Core\Data\Wp;
+
+use RebelCode\Wpra\Core\Data\AbstractDelegateDataSet;
+use RebelCode\Wpra\Core\Data\AliasingDataSet;
+use RebelCode\Wpra\Core\Data\ArrayDataSet;
+use RebelCode\Wpra\Core\Data\DataSetInterface;
+
+/**
+ * A dataset implementation for WordPress plugin information.
+ *
+ * @since [*next-version*]
+ */
+class WpPluginInfoDataSet extends AbstractDelegateDataSet
+{
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $pluginFilePath The path to the plugin's main file.
+     */
+    public function __construct($pluginFilePath)
+    {
+        parent::__construct($this->createInnerDataSet($pluginFilePath));
+    }
+
+    /**
+     * Creates the inner data set.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $pluginFilePath The path to the plugin's main file.
+     *
+     * @return DataSetInterface
+     */
+    protected function createInnerDataSet($pluginFilePath)
+    {
+        $inner = new ArrayDataSet(get_plugin_data($pluginFilePath));
+        $aliased = new AliasingDataSet($inner, [
+            'name' => 'Name',
+            'plugin_uri' => 'PluginURI',
+            'version' => 'Version',
+            'description' => 'Description',
+            'author' => 'Author',
+            'author_uri' => 'AuthorURI',
+            'text_domain' => 'TextDomain',
+            'domain_path' => 'DomainPath',
+            'network' => 'Network',
+            'sitewide' => '_sitewide',
+        ]);
+
+        return $aliased;
+    }
+}
