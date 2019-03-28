607e46e7348bdf8f2cdcbcdc43d0fd4d590c1b2a
Miguel Muscat <miguelmuscat93@gmail.com>
2019-03-28 16:07:04 +0100

    Added logging module

---
+++ src/Modules/LoggerModule.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace RebelCode\Wpra\Core\Modules;
+
+use Psr\Container\ContainerInterface;
+use RebelCode\Wpra\Core\Database\WpdbTable;
+use RebelCode\Wpra\Core\Logger\WpdbLogger;
+
+/**
+ * A module that adds a logger to WP RSS Aggregator.
+ *
+ * @since [*next-version*]
+ */
+class LoggerModule implements ModuleInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getFactories()
+    {
+        return [
+            /*
+             * The main logger instance.
+             *
+             * @since [*next-version*]
+             */
+            'wpra/logs/logger' => function (ContainerInterface $c) {
+                return new WpdbLogger(
+                    $c->get('wpra/logs/log_table'),
+                    $c->get('wpra/logs/log_table_columns'),
+                    $c->get('wpra/logs/log_table_extra')
+                );
+            },
+            /*
+             * The table where logs are stored.
+             *
+             * @since [*next-version*]
+             */
+            'wpra/logs/log_table' => function (ContainerInterface $c) {
+                return new WpdbTable(
+                    $c->get('wpdb'),
+                    $c->get('wpra/logs/log_table_name'),
+                    $c->get('wpra/logs/log_table_schema'),
+                    $c->get('wpra/logs/log_table_primary_key')
+                );
+            },
+            /*
+             * The name of the table where logs are stored.
+             *
+             * @since [*next-version*]
+             */
+            'wpra/logs/log_table_name' => function (ContainerInterface $c) {
+                return 'wprss_logs';
+            },
+            /*
+             * The table columns to use for the log table.
+             *
+             * @since [*next-version*]
+             */
+            'wpra/logs/log_table_schema' => function () {
+                return [
+                    'id' => 'BIGINT NOT NULL AUTO_INCREMENT',
+                    'date' => 'TIMESTAMP DEFAULT CURRENT_TIMESTAMP',
+                    'level' => 'varchar(30) NOT NULL',
+                    'message' => 'text NOT NULL',
+                    'feed_id' => 'varchar(100)',
+                ];
+            },
+            /*
+             * The mapping of log properties to columns.
+             *
+             * @since [*next-version*]
+             */
+            'wpra/logs/log_table_columns' => function () {
+                return [
+                    WpdbLogger::LOG_ID => 'id',
+                    WpdbLogger::LOG_DATE => 'date',
+                    WpdbLogger::LOG_LEVEL => 'level',
+                    WpdbLogger::LOG_MESSAGE => 'message',
+                    'feed_id' => 'feed_id',
+                ];
+            },
+            /*
+             * The log table's primary key.
+             *
+             * @since [*next-version*]
+             */
+            'wpra/logs/log_table_primary_key' => function () {
+                return 'id';
+            },
+            /*
+             * Additional data to include per-log in the log table.
+             *
+             * @since [*next-version*]
+             */
+            'wpra/logs/log_table_extra' => function () {
+                return [
+                    'feed_id' => '',
+                ];
+            },
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getExtensions()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function run(ContainerInterface $c)
+    {
+    }
+}
--- wp-rss-aggregator.php
+++ wp-rss-aggregator.php
@@ -46,6 +46,7 @@ use RebelCode\Wpra\Core\Modules\FeedShortcodeModule;
 use RebelCode\Wpra\Core\Modules\FeedSourcesModule;
 use RebelCode\Wpra\Core\Modules\FeedTemplatesModule;
 use RebelCode\Wpra\Core\Modules\I18nModule;
+use RebelCode\Wpra\Core\Modules\LoggerModule;
 use RebelCode\Wpra\Core\Modules\ModuleInterface;
 use RebelCode\Wpra\Core\Modules\RestApiModule;
 use RebelCode\Wpra\Core\Modules\SettingsModule;
@@ -375,6 +376,7 @@ function wpra_modules()
     return apply_filters('wpra_plugin_modules', [
         'core' => new CoreModule(__FILE__),
         'wp' => new WpModule(),
+        'logging' => new LoggerModule(),
         'feed_sources' => new FeedSourcesModule(),
         'feed_items' => new FeedItemsModule(),
         'blacklist' => new FeedBlacklistModule(),
