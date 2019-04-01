b46cc23f151fad7ec90b27617d7170edcf50e389
Miguel Muscat <miguelmuscat93@gmail.com>
2019-04-01 12:28:14 +0200

    Improved builtin template dataset strategy
    This fixes the errors when writing a template option that did not exist in the general settings.
    Now, all non-general-setting template options are saved in meta data.

--- src/Templates/Feeds/Models/BuiltInFeedTemplate.php
+++ src/Templates/Feeds/Models/BuiltInFeedTemplate.php
@@ -9,9 +9,11 @@ use InvalidArgumentException;
 use OutOfRangeException;
 use RebelCode\Wpra\Core\Data\AliasingDataSet;
 use RebelCode\Wpra\Core\Data\ArrayDataSet;
+use RebelCode\Wpra\Core\Data\DataSetInterface;
 use RebelCode\Wpra\Core\Data\MaskingDataSet;
 use RebelCode\Wpra\Core\Data\MergedDataSet;
 use RebelCode\Wpra\Core\Data\Wp\WpArrayOptionDataSet;
+use RebelCode\Wpra\Core\Data\Wp\WpPostArrayMetaDataSet;
 use WP_Post;
 
 /**
@@ -64,22 +66,49 @@ class BuiltInFeedTemplate extends WpPostFeedTemplate
      */
     protected function createMetaDataSet($postOrId)
     {
-        // Create the DB options dataset and merge it with the defaults
-        $defaultOpts = new ArrayDataSet(wprss_get_default_settings_general());
-        $savedOpts = new WpArrayOptionDataSet(static::WP_OPTION_NAME);
-        $allOpts = new MergedDataSet($savedOpts, $defaultOpts);
-        // Alias the mask the options dataset
-        $aliasedDbOpts = new AliasingDataSet($allOpts, $this->getDbOptionsAliases());
-        $maskedDbOpts = new MaskingDataSet($aliasedDbOpts, $this->getDbOptionsMask(), false);
-        // Wrap the options dataset in another that maps it to an options key
-        $options = new ArrayDataSet([
-            static::OPTIONS_KEY => $maskedDbOpts
-        ]);
+        // Create the DB options dataset
+        $dbOpts = $this->createDbOptionsDataSet();
+        // Get the data set for the posts's template options meta data
+        $metaOpts = new WpPostArrayMetaDataSet($postOrId, static::META_PREFIX . static::OPTIONS_KEY);
 
+        // Merge the DB options and the meta options.
+        // Any options not in the old general settings will be retrieved from meta
+        // Any non-general-settings options that are set will be saved in meta
+        $fullOptions = new MergedDataSet($dbOpts, $metaOpts);
+
+        // Retrieve the full original template meta
         $originalMeta = parent::createMetaDataSet($postOrId);
+        // Create a dataset specifically for the template options
+        $overrideOpts = new ArrayDataSet([
+            static::OPTIONS_KEY => $fullOptions
+        ]);
+        // Override the original meta's `options` with the combined DB-Meta dataset
+        $final = new MergedDataSet($originalMeta, $overrideOpts, [static::OPTIONS_KEY => true]);
 
-        // Merge the original post meta and the options dataset, but explicitly let the options key be overridden
-        return new MergedDataSet($originalMeta, $options, [static::OPTIONS_KEY => true]);
+        return $final;
+    }
+
+    /**
+     * Creates the dataset for the template options that are saved in the WPRA general settings.
+     *
+     * @since [*next-version*]
+     *
+     * @return DataSetInterface
+     */
+    protected function createDbOptionsDataSet()
+    {
+        // Get the default options as a static array dataset
+        $defaultOpts = new ArrayDataSet(wprss_get_default_settings_general());
+        // Get the saved DB options
+        $savedOpts = new WpArrayOptionDataSet(static::WP_OPTION_NAME);
+        // Merge the defaults and the saved options
+        $allOpts = new MergedDataSet($savedOpts, $defaultOpts);
+        // Mask the options to hide the other non-template general settings
+        $maskedOpts = new MaskingDataSet($allOpts, $this->getDbOptionsMask(), false);
+        // Alias the old DB options to the new template option names
+        $aliasedDbOpts = new AliasingDataSet($maskedOpts, $this->getDbOptionsAliases());
+
+        return $aliasedDbOpts;
     }
 
     /**
