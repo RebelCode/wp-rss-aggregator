2c3739c50a82fb08c8531b10b2ca1d1ed3d41acf
Miguel Muscat <miguelmuscat93@gmail.com>
2019-04-15 16:12:15 +0200

    Extracted modular module code from plugin class

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,91 +2,11 @@
 
 namespace RebelCode\Wpra\Core;
 
-use Psr\Container\ContainerInterface;
-use RebelCode\Wpra\Core\Modules\ModuleInterface;
-
 /**
  * The WP RSS Aggregator plugin module.
  *
  * @since [*next-version*]
  */
-class Plugin implements ModuleInterface
+class Plugin extends ModularModule
 {
-    /**
-     * The plugin modules.
-     *
-     * @since [*next-version*]
-     *
-     * @var ModuleInterface[]
-     */
-    protected $modules;
-
-    /**
-     * Constructor.
-     *
-     * @since [*next-version*]
-     *
-     * @param ModuleInterface[] $modules The modules.
-     */
-    public function __construct($modules)
-    {
-        $this->modules = $modules;
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @since [*next-version*]
-     */
-    public function getFactories()
-    {
-        $services = [];
-
-        foreach ($this->modules as $module) {
-            $services = array_merge($services, $module->getFactories());
-        }
-
-        return $services;
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @since [*next-version*]
-     */
-    public function getExtensions()
-    {
-        $extensions = [];
-
-        foreach ($this->modules as $module) {
-            foreach ($module->getExtensions() as $key => $extension) {
-                if (array_key_exists($key, $extensions)) {
-                    $current = $extensions[$key];
-                    $extension = function (ContainerInterface $c, $previous) use ($current, $extension) {
-                        $result1 = $current($c, $previous);
-                        $result2 = $extension($c, $result1);
-
-                        return $result2;
-                    };
-                }
-
-                $extensions[$key] = $extension;
-            }
-        }
-
-        return $extensions;
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @since [*next-version*]
-     */
-    public function run(ContainerInterface $c)
-    {
-        // Run all modules
-        foreach ($this->modules as $module) {
-            $module->run($c);
-        }
-    }
 }
