cf31d3eab36d9d58ea73794d9f6b5a220f2ee2d8
Miguel Muscat <miguelmuscat93@gmail.com>
2019-04-17 14:38:35 +0200

    Added function to check for E&T addon

--- wp-rss-aggregator.php
+++ wp-rss-aggregator.php
@@ -783,7 +783,13 @@ function wprss_get_script_url( $url, $extension = null ) {
     return apply_filters( 'wprss_script_url',  $script_url, $url, $extension );
 }
 
+// Checks if E&T add on is active
+function wprss_is_et_active()
+{
+    return defined('WPRSS_ET_VERSION');
+}
+
 // If ET is active, fallback to the legacy rendering system
 add_filter('wpra/templates/fallback_to_legacy_system', function ($fallback) {
-    return $fallback || defined('WPRSS_ET_VERSION');
+    return $fallback || wprss_is_et_active();
 });
