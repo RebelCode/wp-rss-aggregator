35f53de13cea26a68fdb86fb7fce36c4ecf9c233
Miguel Muscat <miguelmuscat93@gmail.com>
2019-04-18 12:08:19 +0200

    Added E&T discontinued notice in plugins page

--- wp-rss-aggregator.php
+++ wp-rss-aggregator.php
@@ -795,3 +795,24 @@ function wprss_is_et_active()
 add_filter('wpra/templates/fallback_to_legacy_system', function ($fallback) {
     return $fallback || wprss_is_et_active();
 });
+
+// Notice for ET discontinuation
+add_action('after_plugin_row', function ($plugin_file, $plugin_data, $status) {
+    if ($plugin_data['Name'] !== 'WP RSS Aggregator - Excerpts and Thumbnails') {
+        return;
+    }
+
+    echo '<tr class="plugin-update-tr wprss-et-plugin-row-msg">';
+    echo '    <td colspan="3" class="plugin-update colspanchange">';
+    echo '        <div class="update-message notice inline notice-info notice-alt">';
+
+    printf('<p>%1$s <a href="%3$s" target="_blank">%2$s</a></p>',
+        __('The Excerpts & Thumbnails add-on is to be discontinued. Support will be provided until the expiry of your current license.', 'wprss'),
+        __('Click here to learn more.', 'wprss'),
+        'https://www.wprssaggregator.com/excerpts-thumbnails-add-on-to-be-discontinued/'
+    );
+
+    echo '        </div>';
+    echo '    </td>';
+    echo '</tr>';
+}, 10, 3);
