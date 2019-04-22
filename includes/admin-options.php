4d10dfcc0dd01fcc13788ac567b281ca0d7aeafa
Roman Lytvynenko <litvinenko95@gmail.com>
2019-04-22 11:38:06 +0300

    Fixed typo in settings notice block

--- includes/admin-options.php
+++ includes/admin-options.php
@@ -258,7 +258,7 @@
                 'title' => __('The display options for WP RSS Aggregator have now become Templates!', WPRSS_TEXT_DOMAIN),
                 'body' => __('As of version 4.13, we have introduced the concept of templates to replace the'
                     . ' display settings that were previously available on this page. These templates will provide you'
-                    . ' with much more flexibiltiy and new designs. They also come with a revamped <a target="_blank" href="https://kb.wprssaggregator.com/article/54-displaying-imported-items-shortcode">TinyMCE shortcode button</a>'
+                    . ' with much more flexibility and new designs. They also come with a revamped <a target="_blank" href="https://kb.wprssaggregator.com/article/54-displaying-imported-items-shortcode">TinyMCE shortcode button</a>'
                     . ' (for the Classic Editor) and a <a href="https://kb.wprssaggregator.com/article/454-displaying-imported-items-block-gutenberg" target="_blank">brand new block</a>, for those using WP 5.0+ with the Gutenberg block editor!'
                     . '<br/><br/>'
                     . 'Go to “Templates” under “RSS Aggregator” to set up your templates. Please note that the default '
