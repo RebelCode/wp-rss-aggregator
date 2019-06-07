3d245cd95e8c4a4180d6a54480e45be3a98fdb13
Miguel Muscat <miguelmuscat93@gmail.com>
2019-06-07 13:46:43 +0200

    Fixed comma mistake in beacon style registration

--- includes/scripts.php
+++ includes/scripts.php
@@ -230,6 +230,6 @@
         wp_register_style( 'wprss-admin-editor-styles', WPRSS_CSS . 'admin-editor.css', array(), $version );
         wp_register_style( 'wprss-admin-tracking-styles', WPRSS_CSS . 'admin-tracking-styles.css', array(), $version );
         wp_register_style( 'wprss-admin-general-styles', WPRSS_CSS . 'admin-general-styles.css', array(), $version );
-        wp_register_style( 'wprss-hs-beacon-css', WPRSS_CSS, 'beacon.css', array(), $version );
+        wp_register_style( 'wprss-hs-beacon-css', WPRSS_CSS . 'beacon.css', array(), $version );
         wp_register_style( 'jquery-style', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css', array(), $version );
     }
