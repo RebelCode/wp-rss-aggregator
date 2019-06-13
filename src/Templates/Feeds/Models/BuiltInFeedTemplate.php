de28c62ad97072840b32095d30515ca1dcf1f470
Miguel Muscat <miguelmuscat93@gmail.com>
2019-06-13 14:35:42 +0200

    Reworked builtin template using delegator dataset

--- src/Templates/Feeds/Models/BuiltInFeedTemplate.php
+++ src/Templates/Feeds/Models/BuiltInFeedTemplate.php
@@ -2,20 +2,15 @@
 
 namespace RebelCode\Wpra\Core\Templates\Feeds\Models;
 
-use Dhii\Exception\CreateInvalidArgumentExceptionCapableTrait;
-use Dhii\I18n\StringTranslatingTrait;
-use Dhii\Util\Normalization\NormalizeIterableCapableTrait;
-use InvalidArgumentException;
-use OutOfRangeException;
+use RebelCode\Wpra\Core\Data\AbstractDelegateDataSet;
 use RebelCode\Wpra\Core\Data\AliasingDataSet;
 use RebelCode\Wpra\Core\Data\ArrayDataSet;
-use RebelCode\Wpra\Core\Data\CompositeDataSet;
 use RebelCode\Wpra\Core\Data\DataSetInterface;
+use RebelCode\Wpra\Core\Data\DelegatorDataSet;
 use RebelCode\Wpra\Core\Data\MaskingDataSet;
 use RebelCode\Wpra\Core\Data\MergedDataSet;
-use RebelCode\Wpra\Core\Data\Wp\WpArrayOptionDataSet;
+use RebelCode\Wpra\Core\Data\Wp\WpCptDataSet;
 use RebelCode\Wpra\Core\Data\Wp\WpPostArrayMetaDataSet;
-use RebelCode\Wpra\Core\Templates\Feeds\Types\ListTemplateType;
 use WP_Post;
 
 /**
@@ -24,17 +19,8 @@ use WP_Post;
  *
  * @since 4.13
  */
-class BuiltInFeedTemplate extends WpPostFeedTemplate
+class BuiltInFeedTemplate extends AbstractDelegateDataSet
 {
-    /* @since 4.13 */
-    use NormalizeIterableCapableTrait;
-
-    /* @since 4.13 */
-    use CreateInvalidArgumentExceptionCapableTrait;
-
-    /* @since 4.13 */
-    use StringTranslatingTrait;
-
     /**
      * The name of the option from which to retrieve the template settings.
      *
@@ -50,6 +36,13 @@ class BuiltInFeedTemplate extends WpPostFeedTemplate
     const OPTIONS_KEY = 'options';
 
     /**
+     * The meta prefix.
+     *
+     * @since [*next-version*]
+     */
+    const META_PREFIX = 'wprss_template_';
+
+    /**
      * Constructor.
      *
      * @since 4.13
@@ -58,140 +51,139 @@ class BuiltInFeedTemplate extends WpPostFeedTemplate
      */
     public function __construct($postOrId)
     {
-        parent::__construct($postOrId);
+        parent::__construct($this->createDataSet($postOrId));
     }
 
     /**
-     * {@inheritdoc}
+     * Creates the internal data set.
      *
-     * @since 4.13
-     */
-    protected function createMetaDataSet($postOrId)
-    {
-        // Create the settings options dataset
-        $settingsOpts = $this->createSettingsOptionsDataSet();
-        // Get the data set for the posts's template "options" meta key
-        $metaOpts = new WpPostArrayMetaDataSet($postOrId, static::META_PREFIX . static::OPTIONS_KEY);
-        // Merge the meta and settings options with a composite data set.
-        // Data reading prioritizes meta data. If not found, an option will be read from the settings.
-        // Writing is done on both data sets. Iteration is unique.
-        $fullOpts = new CompositeDataSet([$metaOpts, $settingsOpts]);
-
-        // Retrieve the full original template meta
-        $fullPostMeta = parent::createMetaDataSet($postOrId);
-        // Create a dataset specifically for the template options
-        $overrideOpts = new ArrayDataSet([
-            static::OPTIONS_KEY => $fullOpts,
-        ]);
-        // Override the original meta's `options` with the combined Settings-Meta dataset
-        $final = new MergedDataSet($fullPostMeta, $overrideOpts, [static::OPTIONS_KEY => true]);
-
-        return $final;
-    }
-
-    /**
-     * Creates the dataset for the template options that are saved in the WPRA general settings.
+     * @since [*next-version*]
      *
-     * @since 4.13
+     * @param int|WP_Post $postOrId The post instance or ID.
      *
      * @return DataSetInterface
      */
-    protected function createSettingsOptionsDataSet()
+    protected function createDataSet($postOrId)
     {
-        // Get the default options as a static array dataset
-        $defaultOpts = new ArrayDataSet(wprss_get_default_settings_general());
-        // Get the saved settings options
-        $savedOpts = new WpArrayOptionDataSet(static::WP_OPTION_NAME);
-        // Merge the defaults and the saved options
-        $allOpts = new MergedDataSet($savedOpts, $defaultOpts);
-        // Alias the old settings options to the new template option names
-        $aliasedOpts = new AliasingDataSet($allOpts, $this->getSettingsOptionsAliases());
-        // Mask the options to hide the other non-template general settings
-        $maskedOpts = new MaskingDataSet($aliasedOpts, $this->getSettingsOptionsMask(), false);
+        // Create the CPT data set, which only includes the required wp_post fields, which are aliased
+        $cptDataSet = new AliasingDataSet(
+            new WpCptDataSet(
+                $postOrId,
+                static::META_PREFIX,
+                ['ID', 'post_title', 'post_name']
+            ),
+            [
+                'id' => 'ID',
+                'name' => 'post_title',
+                'slug' => 'post_name',
+            ]
+        );
 
-        return $maskedOpts;
-    }
+        // Create the data set for the template options (the "options" key)
+        {
+            // Create the data set for the "options" sub-meta
+            $metaOptsDataSet = new WpPostArrayMetaDataSet($postOrId, static::META_PREFIX . 'options');
+            // Create the data set for the "options" stored in the settings
+            // And wrap it to be able to use the aliases instead of the old settings keys
+            // And mask it to prevent other settings from being read or written to
+            $settingOptsDataSet = new MaskingDataSet(
+                new AliasingDataSet(
+                    wpra_container()->get('wpra/settings/general/dataset'),
+                    $this->getSettingsAliases()
+                ),
+                $this->getSettingsMask(),
+                false
+            );
 
-    /**
-     * {@inheritdoc}
-     *
-     * @since 4.13
-     */
-    protected function set($key, $value)
-    {
-        if ($key !== self::OPTIONS_KEY) {
-            parent::set($key, $value);
-
-            return;
+            // Create the template options delegator data set for the options in meta and settings
+            // This is used to determine which keys are stored in which which data set
+            $optsDelegatorDataSet = new DelegatorDataSet(
+                [
+                    'settings' => $settingOptsDataSet,
+                    'meta' => $metaOptsDataSet,
+                ],
+                DelegatorDataSet::fixedMap($this->getKeyMapping(), 'meta')
+            );
         }
 
-        try {
-            $iterable = $this->_normalizeIterable($value);
-            // If the key is the options key and the value is a collection, override the default behavior to set
-            // each collection entry individually to the template options dataset
-            $dataset = $this->get(self::OPTIONS_KEY);
-            foreach ($iterable as $subKey => $subVal) {
-                $dataset[$subKey] = $subVal;
-            }
-        } catch (InvalidArgumentException $exception) {
-            throw new OutOfRangeException('Cannot set non-collection values to "options"');
-        }
+        // Wrap the opts delegator data set to nest it under the "options" key, with recursive writing
+        // This consists of the post data set, and the meta and settings combined under an "options" key
+        $optsDataSet = new ArrayDataSet(
+            [
+                'options' => $optsDelegatorDataSet,
+            ],
+            true
+        );
+
+        // Create the full data set, explicitly forwarding the options key to the second data set
+        $dataSet = new MergedDataSet(
+            $cptDataSet,
+            $optsDataSet,
+            ['options' => true],
+            MergedDataSet::ITERATE_BOTH
+        );
+
+        return $dataSet;
     }
 
     /**
      * Retrieves the settings options aliases.
      *
-     * @since 4.13
+     * @since [*next-version*]
      *
      * @return string[]
      */
-    protected function getSettingsOptionsAliases()
+    protected function getSettingsAliases()
     {
         return [
-            'limit' => 'feed_limit',
-            'title_max_length' => 'title_limit',
             'title_is_link' => 'title_link',
-            'pagination_type' => 'pagination',
-            'source_enabled' => 'source_enable',
-            'source_prefix' => 'text_preceding_source',
-            'source_is_link' => 'source_link',
-            'author_enabled' => 'authors_enable',
+            'title_max_length' => 'title_limit',
+            'limit' => 'feed_limit',
             'date_enabled' => 'date_enable',
             'date_prefix' => 'text_preceding_date',
             'date_format' => 'date_format',
             'date_use_time_ago' => 'time_ago_format_enable',
-            'links_behavior' => 'open_dd',
+            'source_enabled' => 'source_enable',
+            'source_prefix' => 'text_preceding_source',
+            'source_is_link' => 'source_link',
+            'author_enabled' => 'authors_enable',
+            'pagination_type' => 'pagination',
             'links_nofollow' => 'follow_dd',
+            'links_behavior' => 'open_dd',
             'links_video_embed_page' => 'video_link',
         ];
     }
 
     /**
+     * Retrieves the mapping of data keys to the key of the corresponding data set.
+     *
+     * The settings aliases are used to construct this mapping, since the only explicit mapping we need are for
+     * settings keys. Any other keys will default to the meta data set.
+     *
+     * @since [*next-version*]
+     *
+     * @return array
+     */
+    protected function getKeyMapping()
+    {
+        $keys = array_keys($this->getSettingsAliases());
+        $values = array_fill(0, count($keys), 'settings');
+
+        return array_combine($keys, $values);
+    }
+
+    /**
      * Retrieves the mask for which settings option keys to retain in the dataset.
      *
-     * @since 4.13
+     * @since [*next-version*]
      *
      * @return bool[]
      */
-    protected function getSettingsOptionsMask()
+    protected function getSettingsMask()
     {
-        // Dummy list template type, used to get the options
-        $listType = new ListTemplateType(new ArrayDataSet([]));
-        $listOptions = $listType->getOptions();
+        $keys = array_keys($this->getSettingsAliases());
+        $values = array_fill(0, count($keys), true);
 
-        // Use a "true" mask for every list template type option available
-        $mask = [];
-        foreach ($listOptions as $key => $schema) {
-            $mask[$key] = true;
-        }
-
-        // Allow the standard options through the mask as well
-        $mask['links_behavior'] = true;
-        $mask['links_nofollow'] = true;
-        $mask['links_video_embed_page'] = true;
-        $mask['custom_css_classname'] = true;
-        $mask['className'] = true;
-
-        return $mask;
+        return array_combine($keys, $values);
     }
 }
