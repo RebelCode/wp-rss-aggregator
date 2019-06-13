64e965763479a16010688c2f6864ba2f88a22a23
Miguel Muscat <miguelmuscat93@gmail.com>
2019-06-13 17:15:33 +0200

    Added interface for WordPress extensions

---
+++ src/Wp/WpExtensionInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RebelCode\Wpra\Core\Wp;
+
+/**
+ * Represents a WordPress extension.
+ *
+ * @since [*next-version*]
+ */
+interface WpExtensionInterface
+{
+    /**
+     * Registers the WordPress extension.
+     *
+     * @since [*next-version*]
+     */
+    public function register();
+}
