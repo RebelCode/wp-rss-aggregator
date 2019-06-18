633e817abe4e44377ea5f92e303fc5d214e6d17a
Miguel Muscat <miguelmuscat93@gmail.com>
2019-06-18 11:36:01 +0200

    Lightbox no longer bound to `<a>` elements

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 jQuery( document ).ready( function() { 
 if(jQuery.fn.colorbox) {
-    jQuery( 'a.colorbox' ).colorbox(
+    jQuery( '.colorbox' ).colorbox(
     {iframe:true, width:'80%', height:'80%'}
     );
 }
