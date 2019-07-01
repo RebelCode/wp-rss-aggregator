c2011b51a54d219eacd7eb3210fc9df2a2d04a46
Miguel Muscat <miguelmuscat93@gmail.com>
2019-07-01 10:10:08 +0200

    State column has label and hidden on trash page

--- includes/admin-display.php
+++ includes/admin-display.php
@@ -27,22 +27,25 @@
      * @since 2.0
      */
     function wprss_set_feed_custom_columns( $columns ) {
+        $isTrashPage = filter_input(INPUT_GET, 'post_status') === 'trash';
 
         $columns = array(
             'cb'          =>  '<input type="checkbox" />',
-            'state'       =>  __( '', WPRSS_TEXT_DOMAIN ),
-            'title'       =>  __( 'Name', WPRSS_TEXT_DOMAIN ),
         );
 
-        $columns = apply_filters( 'wprss_set_feed_custom_columns', $columns );
 
-        // Columns to add when feed is not trashed
-        if ( !isset( $_GET['post_status'] ) || $_GET['post_status'] !== 'trash' ) {
+        if (!$isTrashPage) {
+            $columns['state'] =  __( 'State', WPRSS_TEXT_DOMAIN );
+        }
+
+        $columns['title'] = __( 'Name', WPRSS_TEXT_DOMAIN );
+
+        if (!$isTrashPage) {
             $columns['updates'] = __( 'Updates', WPRSS_TEXT_DOMAIN );
             $columns['feed-count'] = __( apply_filters( 'wprss_feed_items_count_column', 'Imported items' ), WPRSS_TEXT_DOMAIN );
         }
 
-        return $columns;
+        return apply_filters( 'wprss_set_feed_custom_columns', $columns );
     }
 
 
