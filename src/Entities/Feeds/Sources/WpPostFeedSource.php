1f7b28f57ef5c4429628a83f49824e9a7110accf
Miguel Muscat <miguelmuscat93@gmail.com>
2019-07-15 17:28:35 +0200

    Improved feed source site_url defaulting

--- src/Entities/Feeds/Sources/WpPostFeedSource.php
+++ src/Entities/Feeds/Sources/WpPostFeedSource.php
@@ -8,6 +8,7 @@ use RebelCode\Wpra\Core\Data\ArrayDataSet;
 use RebelCode\Wpra\Core\Data\DataSetInterface;
 use RebelCode\Wpra\Core\Data\MergedDataSet;
 use RebelCode\Wpra\Core\Data\Wp\WpCptDataSet;
+use SimplePie;
 use WP_Post;
 
 /**
@@ -58,7 +59,17 @@ class WpPostFeedSource extends WpCptDataSet
      */
     protected function createMetaDataSet($postOrId)
     {
-        $meta =  parent::createMetaDataSet($postOrId);
+        $meta = parent::createMetaDataSet($postOrId);
+        $url = isset($meta['url']) ? $meta['url'] : '';
+
+        if (!isset($meta['site_url']) && ($feed = wprss_fetch_feed($url)) instanceof SimplePie) {
+            $meta['site_url'] = $feed->get_permalink();
+            $meta['feed_image'] = $feed->get_image_url();
+        } else {
+            $meta['site_url'] = $url;
+            $meta['feed_image'] = '';
+        }
+
         $defaults = $this->getDefaultMetaData($meta);
         $fullMeta = new MergedDataSet($meta, $defaults);
 
@@ -80,7 +91,6 @@ class WpPostFeedSource extends WpCptDataSet
     protected function getDefaultMetaData($meta)
     {
         return new ArrayDataSet([
-            'site_url' => $meta['url'],
             'import_source' => '0',
             'import_ft_images' => '',
             'download_images' => '0',
