297523e506f4ab79ec268b1e4c82fcccc0d252de
Miguel Muscat <miguelmuscat93@gmail.com>
2019-12-18 11:45:13 +0100

    The "Link source" option is only shown with  E&T

--- includes/admin-metaboxes.php
+++ includes/admin-metaboxes.php
@@ -115,11 +115,13 @@
             'type'  => 'checkbox'
         );
 
-        $wprss_meta_fields[ 'source_link' ] = array(
-            'label' => __( 'Link source', WPRSS_TEXT_DOMAIN ),
-            'id'    => $prefix . 'source_link',
-            'type'  => 'boolean_fallback'
-        );
+        if (wprss_is_et_active()) {
+            $wprss_meta_fields[ 'source_link' ] = array(
+                'label' => __( 'Link source', WPRSS_TEXT_DOMAIN ),
+                'id'    => $prefix . 'source_link',
+                'type'  => 'boolean_fallback'
+            );
+        }
 
         $wprss_meta_fields[ 'import_source' ] = array(
             'label'   => __( 'Use source info', WPRSS_TEXT_DOMAIN ),
