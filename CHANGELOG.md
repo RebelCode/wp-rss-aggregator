941255910f0a03c8e483052e2d1352d98027f4f8
Miguel Muscat <miguelmuscat93@gmail.com>
2020-03-09 15:23:18 +0100

    Updated version number to 4.17.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [[*next-version*]] - YYYY-MM-DD
+## [4.17.4] - YYYY-MM-DD
 ### Changed
 * The default template is now created based on type, not slug.
 
--- wp-rss-aggregator.php
+++ wp-rss-aggregator.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP RSS Aggregator
  * Plugin URI: https://www.wprssaggregator.com/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpraplugin
  * Description: Imports and aggregates multiple RSS Feeds.
- * Version: 4.17.3
+ * Version: 4.17.4
  * Author: RebelCode
  * Author URI: https://www.wprssaggregator.com
  * Text Domain: wprss
@@ -78,7 +78,7 @@ use RebelCode\Wpra\Core\Plugin;
 
 // Set the version number of the plugin.
 if( !defined( 'WPRSS_VERSION' ) )
-    define( 'WPRSS_VERSION', '4.17.3' );
+    define( 'WPRSS_VERSION', '4.17.4' );
 
 if( !defined( 'WPRSS_WP_MIN_VERSION' ) )
     define( 'WPRSS_WP_MIN_VERSION', '4.8' );
