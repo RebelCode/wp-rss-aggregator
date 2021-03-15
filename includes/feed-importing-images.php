4cc712888dd9339ed1ca1dc2cad60ca97e0fa154
Miguel <miguelmuscat93@gmail.com>
2021-03-15 10:14:56 +0100

    Increased time limits for image download

--- includes/feed-importing-images.php
+++ includes/feed-importing-images.php
@@ -703,8 +703,8 @@ function wpra_media_sideload_image($url = null, $post_id = null, $attach = null,
         return new WP_Error('missing', "Need a valid URL and post ID...");
     }
 
-    // Allow 10 seconds prior to beginning the actual download
-    set_time_limit(apply_filters('wpra/images/time_limit/prepare', 10));
+    // Allow 30 seconds prior to beginning the actual download
+    set_time_limit(apply_filters('wpra/images/time_limit/prepare', 30));
 
     // Check if the image already exists in the media library
     $existing = get_posts([
@@ -727,8 +727,8 @@ function wpra_media_sideload_image($url = null, $post_id = null, $attach = null,
             /* @var $img WPRSS_Image_Cache_Image */
             $url = apply_filters('wpra/images/url_to_download', $url);
 
-            // Allow 30 seconds for the download process
-            set_time_limit(apply_filters('wpra/images/time_limit/download', 30));
+            // Allow 1 minute for the download process
+            set_time_limit(apply_filters('wpra/images/time_limit/download', 60));
 
             $img = $images->get($url);
         } catch (Exception $e) {
@@ -858,8 +858,8 @@ function wpra_media_sideload_image($url = null, $post_id = null, $attach = null,
         // For some reason, deep down filesize() returned 0 for the temporary file without this
         clearstatcache(false, $file_array['tmp_name']);
 
-        // Allocate 10 for WordPress to copy and process the image
-        set_time_limit(apply_filters('wpra/images/time_limit/copy', 10));
+        // Allocate 30 for WordPress to copy and process the image
+        set_time_limit(apply_filters('wpra/images/time_limit/copy', 30));
 
         // $post_data can override the items saved to wp_posts table,
         // like post_mime_type, guid, post_parent, post_title, post_content, post_status
