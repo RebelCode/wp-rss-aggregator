cecf0a37a61cc3b01d9736148e5e8fbe2f7cfd02
Omid <omidakingroid@gmail.com>
2025-12-03 17:20:20 +0300

    Fix: Replace deprecated SimplePie setters and remove access to private properties in V4 feed parser

--- includes/feed-processing.php
+++ includes/feed-processing.php
@@ -38,7 +38,9 @@ function wprss_feed_cache_lifetime($seconds)
  */
 function wprss_do_not_cache_feeds(&$feed)
 {
-    $feed->enable_cache(false);
+    if ($feed instanceof \SimplePie\SimplePie) {
+        $feed->enable_cache(false);
+    }
 }
 
 /**
